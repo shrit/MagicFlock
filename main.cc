4306b34749bd269ff924b8e1313f5f835e4b5853
Omar Shrit <shrit@lri.fr>
2018-10-21 16:59:54 +0200

    add initial support for several quadcopters

--- main.cc
+++ main.cc
@@ -24,10 +24,6 @@ extern "C"
 # include <boost/asio/posix/stream_descriptor.hpp>
 # include <boost/asio.hpp>
 
-//# include <ros/ros.h>
-//# include <geometry_msgs/Pose.h>
-
-
 /*  gazebo include */
 
 # include <gazebo/gazebo_config.h>
@@ -46,23 +42,18 @@ using std::chrono::milliseconds;
 using std::chrono::seconds;
 
 /**  
- * TODO: Reput the code in several files URGENT, 
- * TODO: keep the ros commented in the main
- * TODO: Make the client send the position message to the server
- * TODO: Reimplement the server seperatly in a connection file 
  * TODO: Comment all the code 
  * TODO: Manage the error exit
  * TODO: Recover all the info of the telemetry
  * TODO: print well all the output using ncurses
  * TODO: use boost log to create a log, and to create possible statistical output
+ * TODO: the log sould be in different folder, created at the start 
  * TODO: Also understand the normal log provided by the library
+ * TODO: Create a phase like principle in this code with the same content of robotsim
  * TODO: Implement camera receive video, start, and stop, take photo, etc..
  * TODO: Add a timer for data that are sent, also use timer in the client side
  * to ask for data each 50 ms
- * TODO: Try to use opengl instead of ncurses for keyboard command,
- * TODO: Use the opengl graphical interface
 */
-
 
 
 // inline void action_error_exit(ActionResult result, const std::string &message)
@@ -95,6 +86,10 @@ using std::chrono::seconds;
 // }
 
 
+/*
+ * Wait for keyboard input, non-blocking implementation using asio
+*/
+
 template <class Handler>
 void async_wait(boost::asio::posix::stream_descriptor& in, Handler&& handler) {
   in.async_read_some(boost::asio::null_buffers(),
@@ -110,6 +105,10 @@ void async_wait(boost::asio::posix::stream_descriptor& in, Handler&& handler) {
 }
 
 
+/*  Main file: Start one controller by quadcopters, 
+ *  Start ncurses to intercept keyboard keystrokes.
+ */
+
 int main(int argc, char* argv[])
 {
 
@@ -119,34 +118,43 @@ int main(int argc, char* argv[])
         
   boost::asio::io_service	io_service;
 
-  Controller controller;
-
-  DronecodeSDK dc_;
+  /* Create a vector of controllers
+   * Each controller connect to one quadcopters at a time
+   * 
+   */
   
-  controller.connect_to_quad(dc_, settings.get_connection_url());
+  std::vector<Controller> controllers;
 
-  controller.discover_system(dc_);  
-
-  System& system = dc_.system();
   
-  auto telemetry_ = std::make_shared<dronecode_sdk::Telemetry>(system);
-  auto offboard_  = std::make_shared<dronecode_sdk::Offboard>(system);
-  auto action_    = std::make_shared<dronecode_sdk::Action>(system);
+  for(auto i : controllers){
+    Controller controller;
+    DronecodeSDK dc_;
+    //find a hack to pass port numbers to quads
+    controller.connect_to_quad(dc_, settings.get_connection_url());
     
-  controller.set_rate_result(telemetry_);
-  controller.print_position(telemetry_);
-  controller.quad_health(telemetry_);
-  controller.get_position_ned();
+    controller.discover_system(dc_);  
+    
+    System& system = dc_.system();
+    
+    auto telemetry_ = std::make_shared<dronecode_sdk::Telemetry>(system);
+    auto offboard_  = std::make_shared<dronecode_sdk::Offboard>(system);
+    auto action_    = std::make_shared<dronecode_sdk::Action>(system);
+    
+    controller.set_rate_result(telemetry_);
+    controller.print_position(telemetry_);
+    controller.quad_health(telemetry_);    
+    
+    controllers.push_back(controller);
+  }
 
-  //Connect to the server installed in ns3
+  //Subscribe to gazebo topics published by Ns3
 
   ////////////
-  // Gazebo //
-  ////////////
-
-
-  using PubPtr =  gazebo::transport::PublisherPtr ;
+  // Gazebo /
+  ///////////
   
+  
+  using SubPtr = gazebo::transport::SubscriberPtr
  
   gazebo::client::setup(argc, argv);
   
@@ -154,44 +162,14 @@ int main(int argc, char* argv[])
   
   node->Init();
 
-  PubPtr pub = node->Advertise<gazebo::msgs::Vector3d>("~/quad_position/pose");
+  // to subscribe to the 
+  SubPtr sub = node->Subscribe("~/", function_to_call_treat_the_msg);  
+ 
+  /////////////
+  // ncurses //
+  /////////////
 
-  // std::vector<double> msg;
   
-  // msg.push_back(controller.get_position_ned().position.down_m);
-  // msg.push_back(controller.get_position_ned().position.east_m);
-  // msg.push_back(controller.get_position_ned().position.north_m);
-  
-  
-  gazebo::msgs::Vector3d msg;
-  gazebo::msgs::Set(&msg,
-		    ignition::math::Vector3d(controller.get_position_ned().position.down_m,
-					     controller.get_position_ned().position.east_m,
-					     controller.get_position_ned().position.north_m));
-  pub->Publish(msg);  
-  
-  ///////////
-  // ROS   //
-  ///////////
-  
-  //   ros::init(argc, argv, "node");
-  
-   //  ros::NodeHandle node_handle_;
-  
-   //    ros::Publisher pose_pub;
-  
-  //  using pose_msg = geometry_msgs::Pose;
-    
-    
-  // pose_pub = node_handle_.advertise<pose_msg>("/" + ss.str() + "/pose", 1000);
-    
-    // pose_msg pose;							 
-    // pose.position.x = controller.get_position_ned().position.down_m ;	
-    // pose.position.y = controller.get_position_ned().position.east_m;
-    // pose.position.z = controller.get_position_ned().position.north_m;   
-    
-    // pose_pub.publish(pose);
-    
   setlocale(LC_ALL, "");
   
   initscr();
@@ -207,6 +185,7 @@ int main(int argc, char* argv[])
   
   // Capture special keystrokes (including the four arrow keys)
   keypad(stdscr, TRUE);
+  
   refresh();
   
   int ch;
@@ -220,15 +199,15 @@ int main(int argc, char* argv[])
 		  switch (ch) {
 		  case KEY_UP:
 		    printw("key_up");
-		    controller.forward(offboard_);
+		    controllers.at(0).forward(offboard_);
 		    break;
 		  case KEY_DOWN:
 		    printw("key_down");
-		    controller.backward(offboard_);
+		    controllers.at(0).backward(offboard_);
 		    break;
 		  case KEY_LEFT:
 		    printw("key_left");
-		    controller.goLeft(offboard_);
+		    controllers.at(0).goLeft(offboard_);
 		    break;
 		  case KEY_RIGHT:
 		    printw("key_right");
@@ -236,36 +215,36 @@ int main(int argc, char* argv[])
 		    break;
 		  case 'u':    
 		    printw("goUp");
-		    controller.goUp(offboard_);
+		    controllers.at(0).goUp(offboard_);
 		    break;
 		  case 'd':
 		    printw("goDown");
-		    controller.goDown(offboard_);
+		    controllers.at(0).goDown(offboard_);
 		    break;		    
 		  case 't':
 		    printw("take_off");
-		    controller.takeoff(action_);
+		    controllers.at(0).takeoff(action_);
 		    break;
 		  case 'l':
 		    printw("land");
-		    controller.land(action_);
+		    controllers.at(0).land(action_);
 		    break;
 		  case 'a':
 		    printw("arming...");
-		    controller.arm(action_);
+		    controllers.at(0).arm(action_);
 		    sleep_for(seconds(2));
 		    break;
 		  case '+':
 		    printw("turn to right");
-		    controller.turnToRight(offboard_);		    
+		    controllers.at(0).turnToRight(offboard_);		    
 		    break;
 		  case '-':
 		    printw("turn to left");
-		    controller.turnToLeft(offboard_);		    
+		    controllers.at(0).turnToLeft(offboard_);		    
 		    break;		    
 		  case 's':
-		    controller.init_speed(offboard_);
-		    controller.start_offboard_mode(offboard_);
+		    controllers.at(0).init_speed(offboard_);
+		    controllers.at(0).start_offboard_mode(offboard_);
 		    break;
 		    
 		  default:		    
