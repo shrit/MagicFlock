d9aaf87a9285b231e8e0a54185a7d2bb52fc3518
Omar Shrit <shrit@lri.fr>
2018-11-16 15:12:29 +0100

    Replace Ublas with Eigne
    We decided to Replace Ublas with Eigne, it is easier to use
    and to learn, adding namesapce global, and passe the contoller
    vector instead of copying it

--- algo/q_learning.cc
+++ algo/q_learning.cc
@@ -1,92 +1,105 @@
 # include "q_learning.hh"
 
-Q_learning::Q_learning()
-  :matrix_{3,3},
-     rewards_{}
-
+Q_learning::Q_learning(std::vector<std::shared_ptr<Controller>> controllers)
+  :qtable_{4096,5},
+   states_{4096,3},
+   rewards_{}
 {
 
   /* Initlize the matrix for the algorithm */
-  
-  for (unsigned i = 0; i < m.size1 (); ++ i)
-    for (unsigned j = 0; j < m.size2 (); ++ j)
-      m (i, j) = 0;
-  std::cout << m << std::endl;
-      
-  //   for (auto episode : q_values::max_episode){
-  //     std::cout << "Episode" << episode << std::endl;
+  q_tables_ = MatrixXd::Constant(4096, 5, 0);
+  states_   = MatrixXd::Constant(4096, 3, 0);
 
-  //  }
+  run_episodes(controllers);
   
 }
 
-void Q_learning::move_quads_followers_action(Controllers controllers, int action)
+void Q_learning::move_quads_followers_action(std::vector<std::shared_ptr<Controller>> controllers,
+					     int action)
 {
   switch(action){
 
   case 0:
-    controllers.at(1).goLeft(offboard_);
+    controllers.at(1)->goLeft();
     break;
   case 1:
-    controllers.at(1).goRight(offboard_);
+    controllers.at(1)->goRight();
     break;
   case 2:
-    controllers.at(1).forward(offboard_); 
+    controllers.at(1)->forward(); 
     break;
   case 3:
-    controllers.at(1).backward(offboard_);
+    controllers.at(1)->backward();
     break;
   default:
     continue;
   }  
 }
 
-void Q_learning::run_episods(Controllers controllers)
+void Q_learning::run_episods(std::vector<std::shared_ptr<Controller>> controllers)
 {
-
-  gazebo::msg::Vector3d states;
-
   
-  for (auto episode : q_values::max_episode){
+  for (int episode = 0; episode < q_values::max_episode; episode++){
     
-    std::cout << "Episode : " << episode << std::endl;   
-          
-       // declare position to get signal values
+    std::cout << "Episode : " << episode << std::endl;            
+    
+    lt::position position_ = gazebo_.get_quad_positions();
 
-    gazebo_.get_quads_positions();
-    gazebo_.get_quads_rssi();
-
+    /* TODO: Calculate the distance between each quadcopter */
+    
+    states_.row(episode) = gazebo_.get_quads_rssi();
+    
     // put the recoved signls in state tables 
+    
+    for(int steps = 0; steps < q_values::max_step; steps++){
       
-       for(auto steps : q_values::max_step){
-	 controllers.[0].forward(offboard_); // reiterate to move at
-					    // least 10 centometers,
+      controllers[0]->forward(); // reiterate to move at
+      // least 10 centometers,
 					    // or find another
 					    // solution
-
 	   
-	   //get the new signal of strength difference
-	   
-	 states = gazebo_.get_quads_rssi();
-	 in random = rand() % 10;
-
-
-	 int action1;
-	 int action2;
-	 if(random > q_values::epsilon){
-	    action1 = matrix(); // see with ublas how to get the highest values
-	    action2 = matrix();
-	 }
-	 else {
-	   action1 = rand() % 4;
-	   action2 = action1;
-	 }
-	 move_quads_followers_action(controllers, action1);
-	   }      
-
-
-
-  
+      //get the new signal of strength difference
+      
+      states_.row(episode) = gazebo_.get_quads_rssi();
+      in random = std::rand() % 10;
+      
+      int action1;
+      int action2;
+      
+      if(random > q_values::epsilon){
+	Eigen::VectorXd v, v1;
+	v = qtable_.row(episode);
+	v1= qtable_
+	
+	action1 = v.maxCoeff();
+	action2 = matrix();
+      }
+      else {
+	action1 = std::rand() % 4;
+	action2 = action1;
+      }
+      
+      move_quads_followers_action(controllers, action1);
+      move_quads_followers_action(controllers, action2);
+      
+      /*  recalculate the RSSI after moveing the quads  */
+      
+      new_states_.row(episode) = gazebo_.get_quads_rssi();                  
+      
+      
+      /*  Recalculate the distance between quadcopter  */
+      int reward = 50 - dist1;
+      int reward2 = 50 - dist2;
+      
+      qtable_() = (1 - q_values::learning_rate) * qtable_() + q_values::learning_rate * (reward + discount_rate * /*to continue*/);
+      
+      
+      
+      
+     
+    }      
+    
+    
 }
 
 
--- algo/q_learning.hh
+++ algo/q_learning.hh
@@ -1,14 +1,15 @@
-
 /*  Standard C++ includes  */
 # include <iostream>
 # include <vector>
+# include <cstdlib>
+/* Eigne includes */
+# include <Eigen/Dense>
 
-/*  Boost numeric includes */
-# include <boost/numeric/ublas/matrix.hpp>
-# include <boost/numeric/ublas/io.hpp>
+
 
 /* Quadcopter controller includes  */
 # include "../controller.hh"
+# include "../global.hh"
 
 # include "../gazebo.hh"
 
@@ -17,47 +18,70 @@
 
 namespace algo{
 
-namespace q_values{
-
-  const int max_episode = 10000;
-  const int max_step    = 2;
-  const float learning_rate = 0.9;
-  const float discount_rate =0.95;
-
-  const int epsilon = 1;
-  const int min_epsilon = 0;
-  const float decay_rate  = 0.01;
-  
-}
-
-using namespace boost::numeric::ublas;
+  namespace q_values{
     
-
-class Q_learning
-{
-
-public:
-  Q_learning()
-    :qtable_{4096,5},
-     rewards_{}
-    {}
-
-  void init();
-  void run_episods();
-  void move_quads_followers_action(Controllers controllers, int action);
+    const int max_episode = 10000;
+    const int max_step    = 2;
+    const float learning_rate = 0.9;
+    const float discount_rate =0.95;
+    
+    const int epsilon = 1;
+    const int min_epsilon = 0;
+    const float decay_rate  = 0.01;
+    
+  }
+  
+  
+  namespace Eigen{
+    template<class Matrix>
+    void write_binary(const char* filename, const Matrix& matrix){
+      std::ofstream out(filename,ios::out | ios::binary | ios::trunc);
+      typename Matrix::Index rows=matrix.rows(), cols=matrix.cols();
+      out.write((char*) (&rows), sizeof(typename Matrix::Index));
+      out.write((char*) (&cols), sizeof(typename Matrix::Index));
+      out.write((char*) matrix.data(), rows*cols*sizeof(typename Matrix::Scalar) );
+      out.close();
+    }
+    template<class Matrix>
+    void read_binary(const char* filename, Matrix& matrix){
+      std::ifstream in(filename,ios::in | std::ios::binary);
+      typename Matrix::Index rows=0, cols=0;
+      in.read((char*) (&rows),sizeof(typename Matrix::Index));
+      in.read((char*) (&cols),sizeof(typename Matrix::Index));
+    matrix.resize(rows, cols);
+    in.read( (char *) matrix.data() , rows*cols*sizeof(typename Matrix::Scalar) );
+    in.close();
+    }
+  } // Eigen::
+  
+  
+  
+  
+  class Q_learning
+  {
+    
+  public:
+    Q_learning();
+    
+    void init();
+    void run_episods(std::vector<std::shared_ptr<Controller>> controllers);
+    void move_quads_followers_action(std::vector<std::shared_ptr<Controller>> controllers,
+				     int action);
        
-private:
-  
-  matrix<double> qtable_;
-  vector<float> rewards_;
-  
-  Gazebo gazebo_;
-  
-  Controller controller;
-  
+  private:
 
+    Eigne::MatrixXd  qtable_;
+    Eigne::MatrixXd  states_, new_state_;
+
+    Eigne::VectorXd  rewards_;
+    
+    Gazebo gazebo_;  
+  
+    Controller controller_;
+  
+  
  
   
-};
+  };
 
 }
