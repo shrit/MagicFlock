54fdc972561595897c2b7f397c9fa38555f5d041
Omar Shrit <shrit@lri.fr>
2018-11-16 15:21:21 +0100

    Define a namepsace for the entire project

---
+++ global.hh
@@ -0,0 +1,42 @@
+#ifndef GLOBAL_HH_
+#define GLOBAL_HH_
+
+
+
+
+/*  Global name space for the simulation
+    It contain a namespace with several 
+    defintion used in all files 
+*/
+
+namespace local_types {
+
+
+  
+  struct position {
+    
+    double x;
+    double y;
+    double z;
+    
+  };
+  
+  /// Type of a drone's IP
+  using ip_type   = std::string;
+  /// Type of a drone's port
+  using port_type = uint16_t;
+  //Type of drone's conncetion socket
+  using connection_type = std::string;
+
+
+  using topic_name               = std::string;
+
+  using quads_positions          = std::vector<position>;
+
+  using quads_rssi               = std::vector<double>;
+}
+
+
+
+
+#endif 
