e19cb952c7ae6e3d820b3c86e433fbd75b748e7c
Omar Shrit <shrit@lri.fr>
2018-12-14 16:40:09 +0100

    Adding generic implementation of the structs

--- global.hh
+++ global.hh
@@ -1,32 +1,45 @@
 #ifndef GLOBAL_HH_
 #define GLOBAL_HH_
 
+# include <iostream>
+# include <fstream>
+# include <vector>
+# include <iterator>
 
-
+template <typename T>
+std::ostream& operator<< (std::ostream& out, const std::vector<T>& v) {
+  if ( !v.empty() ) {
+    out << '[';
+    std::copy (v.begin(), v.end(), std::ostream_iterator<T>(out, ", "));
+    out << "\b\b]";
+  }
+  return out;
+}
 
 /*  Global name space for the simulation
     It contain a namespace with several 
     defintion used in all files 
 */
 
-
-
-
 namespace local_types {
+
   
+  template <typename T> 
   struct position {
     
-    double x;
-    double y;
-    double z;
+    T x;
+    T y;
+    T z;
     
   };
-
+  
+  template <typename T> 
   struct rssi {
-    double lf1;
-    double lf2;
-    double ff;
+    T lf1;
+    T lf2;
+    T ff;
   };
+  
   
   /// Type of a drone's IP
   using ip_type   = std::string;
@@ -35,13 +48,27 @@ namespace local_types {
   //Type of drone's conncetion socket
   using connection_type = std::string;
 
-
   using topic_name               = std::string;
 
-  using quads_positions          = std::vector<position>;
+  using quads_positions          = std::vector<position<double>>;
 
   using quads_rssi               = std::vector<double>;
 }
+
+template <typename T>
+std::ostream& operator<< (std::ostream& out, const local_types::position<T> p)
+{
+  out << "[ "<< p.x <<", " << p.y <<", " << p.z <<"]"<<"\n";
+  return out;
+}
+  
+template <typename T>
+std::ostream& operator<< (std::ostream& out, const local_types::rssi<T> r)
+{
+  out << "[ "<< r.lf1 <<", " << r.lf2 <<", " << r.ff <<"]"<<"\n";
+  return out;
+}
+
 
 
 
