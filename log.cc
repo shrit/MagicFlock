5ebe34d98ebfe7a8fb3083a6c809d3a0627cbfa2
Omar Shrit <shrit@lri.fr>
2019-01-09 16:09:53 +0100

    Adding a loggin suppor using boost log
    Only used in Algorithm. To be completed for several files

--- log.cc
+++ log.cc
@@ -1,37 +1,69 @@
 #include "log.hh"
 
 
-namespace log {
+std::ostream& operator<< (std::ostream& os, level level)
+{
+    static const char* strings[] =
+      {
+       "Msg",
+       "Dbg",
+       "Wrn",
+       "Err"                
+      };
 
-  void init_logging()
-  {
-    std::shared_ptr< logging::core > core = logging::core::get();
+    if (static_cast< std::size_t >(level) < sizeof(strings) / sizeof(*strings))
+        os << strings[level];
+    else
+        os << static_cast< int >(level);
 
-    std::shared_ptr< sinks::text_multifile_backend > backend =
-      std::make_shared< sinks::text_multifile_backend >();
-
-    // Set up the file naming pattern
-    backend->set_file_name_composer
-      (
-       sinks::file::as_file_name_composer
-       (expr::stream << "logs/" << expr::attr< std::string >("") << ".log")
-       );
-
-    // Wrap it into the frontend and register in the core.
-    // The backend requires synchronization in the frontend.
-    using sink_t =  sinks::synchronous_sink< sinks::text_multifile_backend >;
-    std::shared_ptr<sink_t> sink(new sink_t(backend));
-
-    // Set the formatter
-    sink->set_formatter
-      (
-       expr::stream
-       << "[: " << expr::attr< std::string >("RequestID")
-       << "] " << expr::smessage
-       );
-
-    core->add_sink(sink);
-  }
+    return os;
+}
 
 
-} // namespace log
+void init()
+{
+   logging::add_file_log
+    (
+        keywords::file_name = "Data_set_%N.log",
+        // This makes the sink to write log records that look like this:
+        // 1: [Msg] A normal severity message
+        // 2: [Err] An error severity message
+        keywords::format =
+        (
+	 expr::stream
+	 << "["
+	 << expr::format_date_time< boost::posix_time::ptime >("TimeStamp", "%H:%M:%S")
+	 << "]"
+	 << "["
+	 << expr::attr<level, severity_tag>("Severity")
+	 << "] " << expr::smessage
+	 ),
+	keywords::rotation_size = 10 * 1024 * 1024,    /*10 MB files*/                                 
+	keywords::time_based_rotation = sinks::file::rotation_at_time_point(12, 0, 0)  
+	  
+    );
+   
+}
+
+// int main(int, char*[])
+// {
+//   init();
+  
+//   logging::add_common_attributes();
+
+  
+//   boost::log::sources::severity_logger<level> lg;
+
+//   //  lg.add_attribute();
+    
+//   BOOST_LOG_SEV(lg, Msg) << "A trace severity message";
+//   BOOST_LOG_SEV(lg, Dbg) << "A debug severity message";
+//   BOOST_LOG_SEV(lg, Err) << "An informational severity message";
+//   BOOST_LOG(lg) << "An informational severity message";
+//   BOOST_LOG(lg) << "An informational severity message";
+//   BOOST_LOG(lg) << "A warning severity message";
+//   BOOST_LOG(lg) << "An error severity message";
+//   BOOST_LOG(lg) << "A fatal severity message";
+  
+//   return 0;
+// }
--- log.hh
+++ log.hh
@@ -1,25 +1,37 @@
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/support/date_time.hpp>
 
-# include <boost/log/core.hpp>
-# include <boost/log/trivial.hpp>
-# include <boost/log/expressions.hpp>
-# include <boost/log/sinks/text_file_backend.hpp>
-# include <boost/log/utility/setup/file.hpp>
-# include <boost/log/utility/setup/common_attributes.hpp>
-# include <boost/log/sources/severity_logger.hpp>
-# include <boost/log/sources/record_ostream.hpp>
+
+#define BOOST_LOG_DYN_LINK 1
+namespace logging = boost::log;
+namespace src = boost::log::sources;
+namespace sinks = boost::log::sinks;
+namespace keywords = boost::log::keywords;
+namespace expr = boost::log::expressions;
 
 
-namespace log {
+enum level
+  {
+   Msg,
+   Dbg,
+   Wrn,
+   Err         
+  };
 
-  namespace logging  = boost::log;
-  namespace src      = boost::log::sources;
-  namespace sinks    = boost::log::sinks;
-  namespace keywords = boost::log::keywords;
+struct severity_tag;
 
   /// Initialize the log system
-  void	init_logging();
-  
-  using severity_level = logging::trivial::severity_level;
-  using logger_type    = logging::sources::severity_logger<severity_level>;
+void init();
 
-} // namespace log
+using severity_level = logging::trivial::severity_level;
+using logger_type    = logging::sources::severity_logger<severity_level>;
+
+
