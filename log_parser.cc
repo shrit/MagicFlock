bb2c2090d2b51615ead9d73feaacabf5afaba429
Omar Shrit <shrit@lri.fr>
2019-01-16 13:48:34 +0100

    Parse the entire file into three vector
    first two vector are the input for the perceptron.
    the third vector is the output fot the perceptron.

--- log_parser.cc
+++ log_parser.cc
@@ -14,6 +14,14 @@ struct rssi {
 };
 
 
+
+template <typename T> 
+struct error {
+  T x;
+  T y;
+};
+
+
 template <typename T>
 std::ostream& operator<< (std::ostream& out, const std::vector<T>& v) {
   if ( !v.empty() ) {
@@ -33,7 +41,10 @@ std::ostream& operator<< (std::ostream& out, const rssi<T>& r)
 }
 
 
-void parse_log_file(std::string file_name, rssi<double>& rssi, int& action, std::vector<double>& error)
+void parse_log_file(std::string file_name,
+		    std::vector<rssi<double>>& rssi_,
+		    std::vector<int>& action_,
+		    std::vector<error<double>>& error_)
 {
   std::string line;
   std::ifstream fs;
@@ -51,21 +62,33 @@ void parse_log_file(std::string file_name, rssi<double>& rssi, int& action, std:
       std::string::size_type sz;
       std::replace_if(line.begin(), line.end(), boost::is_any_of(",[]") , ' ');
       boost::split(values, line, boost::is_any_of(" "));
-    
-      rssi.lf1 = std::stod(values.at(1), &sz);
-      rssi.lf2 = std::stod(values.at(3), &sz);
-      rssi.ff  = std::stod(values.at(5), &sz);
-      action   = std::stod(values.at(7), &sz);
-      error.push_back(std::stod(values.at(9), &sz));
-      error.push_back(std::stod(values.at(11), &sz));
+
+      rssi r;
+      
+      r.lf1 = std::stod(values.at(1), &sz);
+      r.lf2 = std::stod(values.at(3), &sz);
+      r.ff  = std::stod(values.at(5), &sz);
+
+      rssi_.push_back(r);
+      
+      action_.push_back(std::stod(values.at(7), &sz));
+
+      error e;
+
+      e.x = std::stod(values.at(9), &sz);
+      e.y = std::stod(values.at(11), &sz);
+
+      error_push_back(e);
+
     } 
 }
 
 int main(int argc, char* argv[])
 {
-  rssi<double> rssi;
-  int action = 0;
-  std::vector<double> error;
+
+  std::vector<rssi<double>> rssi;
+  std::vector<int> action;
+  std::vector<error<double>> error;
   
   parse_log_file("data_sample", rssi, action, error);
 
