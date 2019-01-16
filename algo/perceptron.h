6af2409b8f9513204416f628bc5d1610c1881ee3
Omar Shrit <shrit@lri.fr>
2019-01-16 15:26:24 +0100

    Moving perceptron algorithm to their logical place

--- algo/perceptron.h
+++ algo/perceptron.h
@@ -1,7 +1,12 @@
+/*  C++ STL include */
+
 # include <vector>
 
+/*  local defined values */
 
+# include "global.hh"
 
+namespace lt = local_types;
 
 class Perceptron
 {
@@ -10,9 +15,13 @@ public:
   Perceptron(int input_count, const double learning_rate = 0.1,
 	     const double threshold = 0.5);
   
-  
-  void train(std::vector<>& , unsigned int max_iterations);  
+  template <typename A,  typename B, typename C>  
+  void train(A a, B b,  unsigned int max_iterations);
+
+  template <typename A,  typename B, typename C>
   bool learn(bool expected_result, const std::vector<double> &inputs);
+
+  
   bool Perceptron::get_result(const std::vector<double> &inputs);
   double Perceptron::dot_product(const std::vector<double> &v1,
 				 const std::vector<double> &v2);
