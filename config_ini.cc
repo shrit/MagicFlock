6be4a152900eb4da5601a3f368f5dfbbbfa78b0b
Omar Shrit <shrit@lri.fr>
2019-03-01 15:53:13 +0100

    Parse first part of the ini file

--- config_ini.cc
+++ config_ini.cc
@@ -15,11 +15,44 @@ Configs::Configs()
 void Configs::parse_ini()
 {
 
+  /*Parse Simulation section*/
+  
   number_of_quads_ = std::stoi(ini.get("simulation").get("quad_number"));
+
+  std::string joystick = ini.get("simulation").get("use_joystick");
+  istringstream(joystick) >> std::boolalpha >> joystick_;
+
+  
+  std::string keyboard = ini.get("simulation").get("use_keyboard");
+  istringstream(keyboard) >> std::boolalpha >> keybooard_;
+    
+  speed_ = std::stoi(ini.get("simulation").get("speed"));
+
+
+    /*Parse quadcopter section*/
+  
+  quad_names_.push_back(ini.get("quadcopter_01").get("model_name"));
+  quad_names_.push_back(ini.get("quadcopter_02").get("model_name"));
+  quad_names_.push_back(ini.get("quadcopter_03").get("model_name"));
+
+  ports.push_back(); // verify convert to unsigned integer
+  ports.push_back(); // verify convert to unsigned integer
+  ports.push_back(); // verify convert to unsigned integer
+
+
+  /*Parse algorithm section*/
+  
+  algo_name = ini.get("algorithm").get("name");
+
+  std::string  train = ini.get("algorithm").get("train");
+  istringstream(train) >> std::boolalpha >> train_;
+
+  qtable_file_name_ = ini.get("algorithm").get("qtable");
+  
+  map_file_name_ = ini.get("algorithm").get("map_qtable");
   
   
 }
-
 
 int Configs::quad_number() const
 {
@@ -31,10 +64,16 @@ std::vector<lt::port_type> Configs::quads_ports() const
   return ports_;
 }
 
-std::string Configs::get_file_name() const
+std::string Configs::qtable_file_name() const
 {
-  return file_name_;
+  return qtable_file_name_;
 }
+
+std::string Configs::map_file_name() const
+{
+  return map_file_name_;
+}
+
 
 float Configs::speed() const
 {
--- config_ini.hh
+++ config_ini.hh
@@ -3,7 +3,7 @@
 
 
 # include "ini.hh"
-
+# include "global.hh"
 
 
 class Configs
@@ -14,15 +14,37 @@ public:
   Configs();
 
 
+  std::string qtable_file_name() const;
+  std::string map_file_name() const;
+  
+  int quad_number() const;
 
+  std::vector<lt::port_type> quads_ports() const;
+  
+  float speed() const;
+
+  bool train() const;
+  
 
 
 private:
 
   mINI::INIStructure ini_;
 
+  int number_of_quads_;
   
+  lt::port_type	port_;
 
+
+  std::string qtable_file_name_;
+  std::string map_file_name_;
+  
+  std::vector<lt::port_type> ports_;
+  
+  float speed_;
+  
+  bool train_;
+  
   
 };
 
