027925824ced5acaf927b979148a870e32dabc60
Omar Shrit <shrit@lri.fr>
2019-03-01 17:11:24 +0100

    Add joystick help

--- settings.cc
+++ settings.cc
@@ -7,17 +7,27 @@ void usage(std::ostream& out)
   
   out<< "Usage : " << std ::endl   
      << "To control the quadcopter using keyboard use : " << std::endl
-     << " a : to arm" << std::endl
-     << " t : to takoff" << std::endl
-     << " l : to land" << std::endl
-     << " s : to activate offboard mode" << std::endl
+     << " a : arm" << std::endl
+     << " t : takoff" << std::endl
+     << " l : land" << std::endl
+     << " s : activate offboard mode" << std::endl
      << " key up    : to go forward" << std::endl
      << " key right : to go right" << std::endl
      << " key left  : to go left" << std::endl
      << " key down  : to go backward" << std::endl
      << " + : to turn clock wise" << std::endl
-     << " - : to turn counter clock wise" << std::endl;
-
+     << " - : to turn counter clock wise" << std::endl
+     << "To control the quadcopter using Xbox joystick use : " << std::endl
+     << " A : to arm" << std::endl
+     << " X : takeoff "<< std::endl
+     << " Y : activate offboard mode" << std::endl
+     << " B : land" << std::endl
+     << " Right Stick up    : to go forward" << std::endl
+     << " Right Stick right : to go right" << std::endl
+     << " Right Stick left : to go left" << std::endl
+     << " Right Stick down : to go backword" << std::endl
+     << " Left Stick up    : to go up" << std::endl
+     << " Left Stick right : to go down" << std::endl;
   
 }
 
@@ -28,20 +38,17 @@ Settings::Settings(int argc, char* argv[])
 
   namespace po = boost::program_options;
   po::options_description option("Allowed:");
-
-  train_ = false;
   
   option.add_options()
     ("help,h", "Print this help message and exit" )				
     ("version,v", "Print the current version")
     ("Versbose,V", "Be more verbose")
-    ("ini-file,r", po::value<std::string>(&ini_file_),"Specify the name of the ini file");
+    ("ini-file,n", po::value<std::string>(&ini_file_),"Specify the name of the ini file");
     
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, option), vm);
   po::notify(vm);
-  
-  
+    
   if(vm.count("help")){
     usage(std::cout);
     std::cout << option << std::endl;
@@ -55,10 +62,6 @@ Settings::Settings(int argc, char* argv[])
       
   if(vm.count("Verbose")){
     std::cout << "Debug level increased ";
-  }
-    
-  if(vm.count("train")){
-    train_ = true;    
-  }
+  }    
 
 }
