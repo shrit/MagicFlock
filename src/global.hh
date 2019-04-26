b6db60856a9912848a48922b3a24b75f41d312a6
Omar Shrit <shrit@lri.fr>
2019-04-26 15:28:00 +0200

    Remove the , after printing the last vector of a value
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/global.hh
+++ src/global.hh
@@ -148,7 +148,11 @@ template <typename T>
 std::ostream& operator<< (std::ostream& out, const std::vector<T>& v)
 {
   if ( !v.empty() ) {
-    std::copy (v.begin(), v.end(), std::ostream_iterator<T>(out, ",")); 
+    for (auto&& it : v) { 
+      out << it ; 
+      if (it != v.back() ) 
+	out << ", "; 
+    } 
   }
   return out;
 }
