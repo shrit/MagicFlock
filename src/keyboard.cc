aeb1314c24667c6cd0ced39b49f2dec34032a08b
Omar Shrit <shrit@lri.fr>
2019-05-10 16:46:19 +0200

    Creat a Keyboard API to recover the keyboard touch
    Signed-off-by: Omar Shrit <shrit@lri.fr>

---
+++ src/keyboard.cc
@@ -0,0 +1,67 @@
+#include "keyboard.hh"
+
+Keyboard::Keyboard(int fd)
+{
+  enable_raw_mode(fd);
+}
+
+Keyboard* ptr;
+void call_back_exit(void) {
+  ptr->disable_raw_mode(STDIN_FILENO);
+}
+
+int Keyboard::enable_raw_mode(int fd)
+{
+  struct termios raw;
+  
+  if (raw_mode_) return 0; /* Already enabled. */
+  if (!isatty(STDIN_FILENO)) goto fatal;
+  atexit(call_back_exit);
+  if (tcgetattr(fd,&orig_termios) == -1) goto fatal;
+
+  raw = orig_termios;  /* modify the original mode */
+  /* input modes: no break, no CR to NL, no parity check, no strip char,
+   * no start/stop output control. */
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  /* output modes - disable post processing */
+  raw.c_oflag &= ~(OPOST);
+  /* control modes - set 8 bit chars */
+  raw.c_cflag |= (CS8);
+  /* local modes - choing off, canonical off, no extended functions,
+   * no signal chars (^Z,^C) */
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  /* control chars - set return condition: min number of bytes and timer. */
+  raw.c_cc[VMIN] = 0; /* Return each byte, or zero for timeout. */
+  raw.c_cc[VTIME] = 1; /* 100 ms timeout (unit is tens of second). */
+
+  /* put terminal in raw mode after flushing */
+  if (tcsetattr(fd,TCSAFLUSH,&raw) < 0) goto fatal;
+  raw_mode_ = 1;
+  return 0;
+
+ fatal:
+  errno = ENOTTY;
+  return -1;
+  
+}
+
+void Keyboard::disable_raw_mode(int fd)
+{
+  /* Don't even check the return value as it's too late. */
+  if (raw_mode_) {
+    tcsetattr(fd,TCSAFLUSH,&orig_termios);
+    raw_mode_ = 0;
+  }  
+}
+
+int Keyboard::poll_event(int fd)
+{
+  int nread;
+  char c;
+  while ((nread = read(fd,&c,1)) == 0);
+  if (nread == -1)
+    exit(1);
+  
+  return c;
+  
+}
---
+++ src/keyboard.hh
@@ -0,0 +1,61 @@
+/*  C includes */
+extern "C" {
+#include <termios.h>
+#include <cstdlib>
+#include <errno.h>
+}
+/* Posix includes */
+#include <unistd.h>
+
+/* C++ includes */
+#include <memory>
+
+static struct termios orig_termios; /* In order to restore at exit.*/
+
+class Keyboard {
+
+public:
+  
+  Keyboard(int fd);  
+  
+  int enable_raw_mode(int fd);
+  void disable_raw_mode(int fd);  
+    
+  int poll_event(int fd);
+  
+  enum class Special_Keys {
+			   KEY_NULL = 0,       
+			   CTRL_C = 3,         
+			   CTRL_D = 4,         
+			   CTRL_F = 6,         
+			   CTRL_H = 8,         
+			   TAB = 9,            
+			   CTRL_L = 12,        
+			   ENTER = 13,         
+			   CTRL_Q = 17,        
+			   CTRL_S = 19,        
+			   CTRL_U = 21,        
+			   ESC = 27,           
+			   BACKSPACE =  127,  
+			   KEY_LEFT = 1000,
+			   KEY_RIGHT,
+			   KEY_UP,
+			   KEY_DOWN,
+			   DEL_KEY,
+			   HOME_KEY,
+			   END_KEY,
+			   PAGE_UP,
+			   PAGE_DOWN,
+  };
+  
+
+  Keyboard(Keyboard const&) = delete;  
+  
+  Keyboard(Keyboard &&) = default;
+  
+
+private:  
+
+  bool raw_mode_;
+  
+};
