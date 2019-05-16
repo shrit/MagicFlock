fbfee30f1a752f24b7e05036afdb7fbaafd544d3
Omar Shrit <shrit@lri.fr>
2019-05-16 17:53:44 +0200

    Ready to return signal strength, need to compile to findout
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/algo/propagation_mode.hxx
+++ src/algo/propagation_mode.hxx
@@ -1,6 +1,9 @@
 Propagation_model<simulator_t>()::Propagation_model()
   :sim_interface_(std::move(sim_interface))
-{}
+{
+  changel_to_frequency();
+  wave_length();  
+}
 
 template <typename simulator_t,
 	  typename T>
@@ -12,7 +15,28 @@ convert_to_distance(T receiver_power)
   T distance = wave_length_ /(4*PI) *
     std::sqrt(transmitter_power_ * transmitter_gain_ * receiver_gain_
 	      / receiver_power * system_loss_);
+  /*  Now we have the 3D disance between two points, So far we need to
+   convert it to 2D since our nn model take on account only 2D
+   distances */
+  double total_heights = sim_interface_->positions().f2.z;
+  double original_heights = sim_interface_->positions().f1.z;
+
+  double diff_heights = total_heights - original_heights;
+  
+  distance =  mtools_.pythagore_leg(diff_heights, distance);
+  
   return distance;  
+}
+
+template <typename simulator_t>
+lt::triangle<T> Propagation_model<simulator_t>::
+distances_2D()
+{
+  lt::triangle<T> dist;
+  dist.f1 = convert_to_distance (sim_interface_.rssi().f1) ;
+  dist.f2 = convert_to_distance (sim_interface_.rssi().f2) ;
+  dist.f3 = convert_to_distance (sim_interface_.rssi().f3) ;
+  return dist;
 }
 
 template <typename simulator_t,
--- src/algo/propagation_model.hh
+++ src/algo/propagation_model.hh
@@ -2,7 +2,9 @@
 
 # include <cmath>
 
+# include "../global.hh"
 # include "../log.hh"
+# include "../math_tools.hh"
 
 constexpr double PI           = 3.14159265358979323846;  /* pi */
 constexpr double speed_of_light = 299792458;
@@ -15,23 +17,27 @@ public:
   Propagation_model(std::shared_ptr<simulator_t> sim_interface_);
   
   template <typename T>
-  T convert_to_distance(T receiver_power);
-  
-  template <typename T>
   T dbm_to_watt(T value);
     
+  lt::triangle<double> distances_2D(T receiver_power);
+  
   void channel_to_frequencey(int channel);
         
   void wave_length();
 
 private:
   
+  template <typename T>
+  T convert_to_distance();
+    
   double frequency_; // in HZ
   double wave_length_;
   double transmitter_power_; // in Watt
   double transmitter_gain_ ;
   double receiver_gain_ ;
   double system_loss_;
+
+  Math_tools mtools_;
   
   std::shared_ptr<simulator_t> sim_interface_;
   
