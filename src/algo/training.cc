992128819222084414eae316ec08335d5b19057d
Omar Shrit <shrit@lri.fr>
2019-08-24 12:38:08 +0200

    Adding regression network
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/algo/training.cc
+++ src/algo/training.cc
@@ -18,6 +18,23 @@ double Train::accuracy(const arma::Row<size_t>& predLabels,
   return (double)success / (double)LabelY.n_cols * 100.0;
 }
 
+/*  HACK, Waiting until callback pull request to be merged into ensmallen
+    library, meanwhile this calculation is used to print the value of the 
+    loss during the training. Print Loss callback in going to be merged
+    in the next months*/
+double Train::accuracy_mse(const arma::mat& predLabels, const arma::mat& LabelY)
+{
+  // Calculating how many predicted classes are coincide with real labels.
+  double sum = 0;
+  for (size_t j = 0; j < LabelY.n_cols; j++) {
+
+    double value = std::pow((predLabels(j) - LabelY(j)), 2);
+    sum = sum + value;
+  }
+  return std::sqrt(sum / predLabels.n_cols);
+}
+
+/*  Used only for classification problem to compare values of several classes */
 arma::Row<size_t> Train::getLabels(const arma::mat& predOut)
 {
   arma::Row<size_t> pred(predOut.n_cols);
@@ -28,47 +45,43 @@ arma::Row<size_t> Train::getLabels(const arma::mat& predOut)
     pred(j) = arma::as_scalar(arma::find(
 					 arma::max(predOut.col(j)) == predOut.col(j), 1)) + 1;
   }
-
   return pred;
 }
 
 void Train::load_data_set(std::string&& dataset_file)
-{
-
-  /*  We need to figure a way to load dataset from a file or
-      to get the data set from the generator */
-
+{  
   // Load the training set.
-
   mlpack::data::Load(dataset_file, dataset_, true);
-
   mlpack::data::Split(dataset_, trainset_, testset_, ratio_);
-  
-  // Split the labels from the training set.
-  trainData_ = trainset_.submat(0, 0,
-			     dataset_.n_rows - 5,
-			     dataset_.n_cols - 1);
-
-  // Split the data from the training set.
-  trainlabel_ = trainset_.submat(dataset_.n_rows - 4, 0,
-			       dataset_.n_rows - 1, dataset_.n_cols - 1);
-
-
-  testData_ = testset_.submat(0, 0,
-			    testset_.n_rows - 5,
-			    testset_.n_cols - 1);
-
-  testlabel_ = testset_.submat(testset_.n_rows - 4, 0,
-			     testset_.n_rows - 1,
-			     testset_.n_cols - 1);
-
 }
 
-void Train::run()
+/* This function define the number of columns used for features and
+   labels. Note that, mlpack is column major. Thus, rows are columns   
+   @param x is the number of columns used for label*/
+void Train::define_label_column_size(int x) {
+  // Split the labels from the training set.
+  train_features_ = trainset_.submat(0, 0,
+				     dataset_.n_rows - ( x + 1),
+				     dataset_.n_cols - 1);
+  
+  // Split the data from the training set.
+  train_labels_ = trainset_.submat(dataset_.n_rows - x, 0,
+				  dataset_.n_rows - 1, dataset_.n_cols - 1);
+  
+  test_features_ = testset_.submat(0, 0,
+				   testset_.n_rows - (x + 1),
+				   testset_.n_cols - 1);
+  
+  test_labels_ = testset_.submat(testset_.n_rows - x, 0,
+				 testset_.n_rows - 1, testset_.n_cols - 1);
+}
+
+void Train::classification()
 {
+  define_label_column_size(4);
   mlpack::ann::FFN<mlpack::ann::SigmoidCrossEntropyError<>,
 		   mlpack::ann::RandomInitialization> model;
-  model.Add<mlpack::ann::Linear<> >(trainData_.n_rows, 200);
+  model.Add<mlpack::ann::Linear<> >(train_features_.n_rows, 200);
   model.Add<mlpack::ann::LeakyReLU<> >();
   model.Add<mlpack::ann::Linear<> >(200, 200);
   model.Add<mlpack::ann::LeakyReLU<> >();
@@ -84,17 +97,17 @@ void Train::run()
   // Train the model.
   for (int i = 0; i < 1000; ++i) {
     LogInfo() << "training...";
-    model.Train(trainData_,
-		trainlabel_,
+    model.Train(train_features_,
+		train_labels_,
 		optimizer);
 
     optimizer.ResetPolicy() = false;
 
     arma::mat pred;
-    model.Predict(trainData_, pred);
+    model.Predict(train_features_, pred);
     // Calculate accuracy on training data points.
     arma::Row<size_t> predLabels = getLabels(pred);
-    arma::Row<size_t> YLabels = getLabels(trainlabel_);
+    arma::Row<size_t> YLabels = getLabels(train_labels_);
 
     double trainAccuracy = accuracy(predLabels, YLabels);
 
@@ -102,17 +115,16 @@ void Train::run()
 	      << ":\tTraining Accuracy = "<< trainAccuracy
 	      << "%";
   }
-
   LogInfo() << "Training Finished...";
   LogInfo() << "Test with Independent part...";
 
   arma::mat predtest;
 
-  model.Predict(testData_, predtest);
+  model.Predict(test_features_, predtest);
 
   arma::Row<size_t> predtestlabel = getLabels(predtest);
 
-  arma::Row<size_t> YTestlabels = getLabels(testlabel_);
+  arma::Row<size_t> YTestlabels = getLabels(test_labels_);
 
   double testAccuracy = accuracy(predtestlabel, YTestlabels);
 
@@ -121,5 +133,69 @@ void Train::run()
   predtest = predtest.t();
   predtest.save("result.csv", arma::raw_ascii);
 
-  mlpack::data::Save("model.xml", "model", model, false);
+  mlpack::data::Save("model.xml", "model", model, false);  
+}
+
+void Train::regression()
+{
+  define_label_column_size(1);
+  mlpack::ann::FFN<mlpack::ann::MeanSquaredError<>,
+		   mlpack::ann::RandomInitialization> model;
+  model.Add<mlpack::ann::Linear<> >(train_features_.n_rows, 200);
+  model.Add<mlpack::ann::LeakyReLU<> >();
+  model.Add<mlpack::ann::Linear<> >(200, 200);
+  model.Add<mlpack::ann::LeakyReLU<> >();
+  model.Add<mlpack::ann::Dropout<> >(0.5);
+  model.Add<mlpack::ann::Linear<> >(200, 200);
+  model.Add<mlpack::ann::LeakyReLU<> >();
+  model.Add<mlpack::ann::Dropout<> >(0.5);
+  model.Add<mlpack::ann::Linear<> >(200, 1);
+
+  ens::AdamType<ens::AdamUpdate> optimizer;
+
+  // Train the model.
+  for (int i = 0; i < 100; ++i) {
+    std::cout << "training..." << std::endl;
+    model.Train(train_features_,
+		train_labels_,
+		optimizer);
+      
+    optimizer.ResetPolicy() = false;
+      
+    arma::mat pred;
+    model.Predict(train_features_, pred);
+    // Calculate accuracy on training data points.
+    arma::Row<size_t> predLabels = getLabels(pred);
+    arma::Row<size_t> YLabels = getLabels(train_labels_);
+      
+    double trainAccuracy = accuracy_mse(pred, train_labels_);       
+      
+    std::cout << "Epoch " << i
+	      << ":\t Loss: "<< trainAccuracy
+	      << std::endl;
+  }
+  LogInfo() << "Training Finished...";
+  LogInfo() << "Test with Independent part...";
+  
+  arma::mat predtest;
+    
+  model.Predict(test_features_, predtest);      
+ 
+  double testAccuracy = accuracy_mse(predtest, test_labels_);
+    
+  std::cout << "LOSS  = "<< testAccuracy<< "%,"
+	    << std::endl;
+    
+  predtest = predtest.t();
+  predtest.save("result.csv", arma::raw_ascii);    
+  mlpack::data::Save("model.txt", "model", model, false);  
+}
+
+void Train::run(const Settings& settings)
+{
+  if (settings.classification()) {
+    classification();    
+  } else if (settings.regression()) {
+    regression();
+  }      
 }
--- src/algo/training.hh
+++ src/algo/training.hh
@@ -2,11 +2,13 @@
 #include <mlpack/core/data/split_data.hpp>
 #include <mlpack/methods/ann/layer/layer.hpp>
 #include <mlpack/methods/ann/loss_functions/sigmoid_cross_entropy_error.hpp>
+#include <mlpack/methods/ann/loss_functions/mean_squared_error.hpp>
 #include <mlpack/methods/ann/ffn.hpp>
 #include <ensmallen_bits/adam/adam_update.hpp>
 #include <armadillo>
 
 #include "../log.hh"
+#include "../settings.hh"
 
 class Train {
 
@@ -16,13 +18,19 @@ public:
 
   double accuracy(const arma::Row<size_t>& predLabels,
 		  const arma::Row<size_t>& LabelY);
-
+  
+  double accuracy_mse(const arma::mat& predLabels,
+		      const arma::mat& LabelY);
+  
   arma::Row<size_t> getLabels(const arma::mat& predOut);
 
   void load_data_set(std::string&& dataset_file);
-
-  void run();
-
+  void define_label_column_size(int x);
+  
+  void run(const Settings& settings);
+  void classification();
+  void regression();
+  
   Train(Train const&) = delete;
 
   Train(Train &&) = default;
@@ -34,11 +42,11 @@ private:
   arma::mat trainset_;
   arma::mat testset_;
   
-  arma::mat trainData_;
-  arma::mat trainlabel_;
+  arma::mat train_features_;
+  arma::mat train_labels_;
 
-  arma::mat testData_;
-  arma::mat testlabel_;
+  arma::mat test_features_;
+  arma::mat test_labels_;
 
   double ratio_ = 0.1;
 
