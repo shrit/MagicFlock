85e26cd4e9ccf52d4ee57c7991ca2106acad1400
Omar Shrit <shrit@lri.fr>
2019-11-07 15:09:18 +0100

    Move prediction function to a new prediction class
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/algo/supervised_learning.hh
+++ src/algo/supervised_learning.hh
@@ -10,14 +10,6 @@
 # include <tuple>
 # include <vector>
 
-/*  MLPack includes */
-#include <mlpack/core.hpp>
-#include <mlpack/methods/ann/layer/layer.hpp>
-#include <mlpack/methods/ann/loss_functions/sigmoid_cross_entropy_error.hpp>
-#include <mlpack/methods/ann/loss_functions/mean_squared_error.hpp>
-#include <mlpack/methods/ann/ffn.hpp>
-#include <ensmallen_bits/adam/adam_update.hpp>
-
 /*  Armadillo includes  */
 # include <armadillo>
 
@@ -27,6 +19,7 @@
 # include "../gazebo.hh"
 # include "../log.hh"
 # include "../math_tools.hh"
+# include "predictor.hh"
 # include "quadcopter.hh"
 # include "../swarm_device.hh"
 
@@ -39,25 +32,9 @@ class Supervised_learning
 public:
   Supervised_learning(std::vector<std::shared_ptr<flight_controller_t>> iris_x,
 		      std::shared_ptr<simulator_t> gzs);
-
-  std::vector<double>
-  estimate_action_from_distance(arma::mat matrix);
-
+  
   void generate_trajectory_using_model(bool random_leader_action,
 				       bool stop_down_action);
-  
-  int index_of_best_action_classification(arma::mat matrix);
-  int index_of_best_action_regression(arma::mat matrix);
-  
-  arma::mat
-  create_absolute_features_matrix(std::vector<Quadcopter::Action> actions);
-
-  arma::mat
-  create_estimated_features_matrix(std::vector<Quadcopter::Action> actions);
-
-  std::tuple<arma::mat, arma::uword> predictor(arma::mat features);
-  
-  double real_time_loss(std::tuple<arma::mat, arma::uword> matrix_best_action);
   
   void run(const Settings& settings);
 
