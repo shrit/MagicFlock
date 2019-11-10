a5f0b78a761157a00b76d3d50baff517c98cf761
Omar Shrit <shrit@lri.fr>
2019-11-10 18:20:40 +0100

    Move reward from quadrotor class
    Signed-off-by: Omar Shrit <shrit@lri.fr>

---
+++ src/algo/reward.cc
@@ -0,0 +1,28 @@
+# include "reward.hh"
+
+Reward::Reward Reward::
+evaluator_current_state(const lt::triangle<double>& old_dist,
+			const lt::triangle<double>& new_dist)
+{
+  LogInfo() << "F1 differences: " << std::fabs(old_dist.f1 - new_dist.f1);
+  LogInfo() << "F2 differences: " << std::fabs(old_dist.f2 - new_dist.f2);
+
+  double diff_f1 = std::fabs(old_dist.f1 - new_dist.f1);
+  double diff_f2 = std::fabs(old_dist.f2 - new_dist.f2);
+
+  Reward reward = Reward::Unknown;
+  
+  if (0.5  > diff_f1 + diff_f2 ) {
+    reward = Reward::very_good;
+  } else if ( 1.0  > diff_f1 + diff_f2 and
+	      diff_f1 + diff_f2  > 0.5 ) {
+    reward = Reward::good;
+  } else if ( 1.5  > diff_f1 + diff_f2 and
+	      diff_f1 + diff_f2  > 1.0 ) {
+    reward = Reward::bad;
+  } else if ( 2.0  > diff_f1 + diff_f2 and
+	      diff_f1 + diff_f2  > 1.5 ) {
+    reward = Reward::very_bad;
+  }
+  return reward;
+}
---
+++ src/algo/reward.hh
@@ -0,0 +1,25 @@
+#pragma once
+
+
+# include "../global.hh"
+
+namespace lt = local_types;
+
+class Reward {
+
+public:
+
+    enum class Reward
+    {
+     very_good,
+     good,
+     bad,
+     very_bad,
+     Unknown,
+    };
+  
+  Reward evaluate_current_state(const lt::triangle<double>& old_dist,
+				const lt::triangle<double>& new_dist);
+  
+  
+};
