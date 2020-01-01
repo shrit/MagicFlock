175dab0f1f21d1a7ada8f58d4b93cb163e661857
Omar Shrit <shrit@lri.fr>
2020-01-01 17:38:03 +0100

    Addin output functions, correct some stuff
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/evaluate_model.cc
+++ src/evaluate_model.cc
@@ -1,9 +1,15 @@
 # include "../evaluate_model.hh"
 
 
-EvaluateModel::EvaluateModel(){}
-
-
+EvaluateModel::EvaluateModel()
+	:count_both_actions_(0),
+	 count_follower_1_(0),
+	 count_follower_2_(0),
+	 count_bad_action_f1_(0),
+	 count_bad_action_f2_(0),
+	 count_not_bad_f1_(0),
+	 count_bad_action_f2_(0)
+{}
 
 void EvaluateModel::input(Actions::Action leader_action, 
 												 Actions::Action follower_1_action,
@@ -20,39 +26,41 @@ void EvaluateModel::input(Actions::Action leader_action,
 				count_both_action_++;
 	}
 	if (leader_action != follower_1_action) {
-		evaluate_not_similar_actions(leader_action, follower_1_action)
-	} else if () {
-
-		
-	} 
+		std::tie(count_not_bad_f1_, count_bad         ) = evaluate_not_similar_actions(leader_action, follower_1_action);
+	} else if (leader_action != follower_2_action) {
+		std::tie () = evaluate_not_similar_actions(leader_action, follower_2_action);		
+	}
 }
 
-void EvaluateModel::evaluate_not_similar_actions(Actions::Action leader_action, 
-																								 Actions::Action follower_action);
+std::tuple<int, int> EvaluateModel::
+evaluate_not_similar_actions(Actions::Action leader_action, 
+														 Actions::Action follower_action);
 {
 	if ((leader_action == Actions::Action::forward and
 	    follower_action == Actions::Action::left)
 	    or  (leader_action == Actions::Action::left and
 	    follower_action == Actions::Action::forward)) {
-			count_not_bad_f1++;
+			count_not_bad++;
 	} else if ((leader_action == Actions::Action::backward and
 	    follower_action == Actions::Action::right)
 	    or  (leader_action == Actions::Action::right and
 	    follower_action == Actions::Action::backward)) {
-			count_not_bad_f1++;
+			count_not_bad++;
 	}	else {
 		count_bad_action++;
 	}
-
+	return std::make_tuple<int, int>(count_not_bad, count_bad_action);
 }
 
 std::vector<double> EvaluateModel::output()
 {
-
-
-
-
-
-
-
+	std::vector<double> output(7);
+	output.at(0) = count_both_actions_;
+	output.at(1) = count_follower_1_;
+	output.at(2) = count_follower_2_;
+	output.at(3) = count_bad_action_f1_;
+	output.at(4) = count_bad_action_f2_;
+	output.at(5) = count_not_bad_f1_;
+	output.at(6) = count_not_bad_f2_;
+	return output;
 }
--- src/include/evaluate_model.hh
+++ src/include/evaluate_model.hh
@@ -15,20 +15,21 @@ public:
 						 Actions::Action follower_1_action,
 						 Actions::Action follower_2_action);
 
-	void evaluate_not_similar_actions(Actions::Action leader_action, 
+	std::tuple<int, int> evaluate_not_similar_actions(Actions::Action leader_action, 
 																	 Actions::Action follower_1_action,
 																	 Actions::Action follower_2_action);
 
   std::vector<double> output();
 
 private:
-	int count_follower_1_;
+
 	int count_both_actions_;
+	int count_follower_1_;
 	int count_follower_2_;
-	int count_bad_action_;
+	int count_bad_action_f1_;
+	int count_bad_action_f2_;
 	int count_not_bad_f1_;
 	int count_not_bad_f2_;
-
 };
 
 
