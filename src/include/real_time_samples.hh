4da372c52f460d3a877bab3325926eeccf9e420b
Omar Shrit <shrit@lri.fr>
2020-01-15 14:47:35 +0100

    Adding a real time sampler to recover values at high rate
    Signed-off-by: Omar Shrit <shrit@lri.fr>

---
+++ src/include/real_time_samples.hh
@@ -0,0 +1,18 @@
+#include <chrono>
+#include <functional>
+#include <future>
+
+class RTSamples
+{
+public:
+  RTSamples();
+  ~RTSamples();
+
+  void stop();
+  void start(int interval, std::function<void(void)> func);
+  bool is_running() const noexcept;
+
+private:
+  std::atomic<bool> _execute;
+  std::thread _thd;
+};
---
+++ src/real_time_samples.cc
@@ -0,0 +1,41 @@
+#include "include/real_time_samples.hh"
+
+RTSamples::RTSamples()
+  : _execute(false)
+{}
+
+RTSamples::~RTSamples()
+{
+  if (_execute.load(std::memory_order_acquire)) {
+    stop();
+  };
+}
+
+void
+RTSamples::stop()
+{
+  _execute.store(false, std::memory_order_release);
+  if (_thd.joinable())
+    _thd.join();
+}
+
+void
+RTSamples::start(int interval, std::function<void(void)> func)
+{
+  if (_execute.load(std::memory_order_acquire)) {
+    stop();
+  };
+  _execute.store(true, std::memory_order_release);
+  _thd = std::thread([this, interval, func]() {
+    while (_execute.load(std::memory_order_acquire)) {
+      func();
+      std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+    }
+  });
+}
+
+bool
+RTSamples::is_running() const noexcept
+{
+  return (_execute.load(std::memory_order_acquire) && _thd.joinable());
+}
