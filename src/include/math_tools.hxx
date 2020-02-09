f6246587dca32071f9ed624d72c48f8a74cd2155
Omar Shrit <shrit@lri.fr>
2020-02-09 16:11:39 +0100

    Fix and into or
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/include/math_tools.hxx
+++ src/include/math_tools.hxx
@@ -61,19 +61,19 @@ Math_tools::is_good_shape(unsigned int id,
                           std::vector<unsigned int> nearest_neighbors,
                           std::vector<lt::position3D<T>> positions)
 {
-  bool value = true;
+  bool good_shape = true;
   std::vector<double> distances =
     map_to_vector(distances_to_neighbors(id, nearest_neighbors, positions));
   logger::logger_->debug("Distances to other quadrotors: {} ", distances);
   if (std::any_of(distances.begin(), distances.end(), [&](const double& i) {
-        if ((i < lower_threshold_) and (i > upper_threshold_)) {
+        if ((i < lower_threshold_) or (i > upper_threshold_)) {
           return true;
         } else
           return false;
       })) {
-    value = false;
+    good_shape = false;
   }
-  return value;
+  return good_shape;
 }
 
 template<typename T>
