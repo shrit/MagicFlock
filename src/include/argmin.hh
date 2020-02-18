1c3bb46f44d032ecd105d65e9eb97987640b7803
Omar Shrit <shrit@lri.fr>
2020-02-18 17:29:54 +0100

    Adding argmin with out a specific column
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/include/argmin.hh
+++ src/include/argmin.hh
@@ -6,9 +6,11 @@ template<class InputType = arma::mat, class OutputType = arma::uword>
 class Argmin
 {
 public:
-    Argmin(InputType mat, InputType mat2);
-    OutputType result() const;
-    
+  Argmin(InputType mat, InputType mat2);
+  Argmin(InputType mat, InputType mat2, arma::uword remove_col);
+
+  OutputType result() const;
+
 private:
   OutputType argmin_result_ = 0;
 };
--- src/include/argmin.hxx
+++ src/include/argmin.hxx
@@ -5,6 +5,22 @@ Argmin<InputType, OutputType>::Argmin(InputType mat, InputType mat2)
 {
   InputType mat3 = mat - mat2;
   mat3 = arma::abs(mat3);
+  //logger::logger->info("Final prediction matrix {}", mat3);
+  arma::colvec vec = arma::sum(mat3, 1);
+  vec = arma::reverse(vec);
+  logger::logger_->info("Sum of distances: {}", vec);
+  argmin_result_ = vec.index_min();
+}
+
+template<class InputType, class OutputType>
+Argmin<InputType, OutputType>::Argmin(InputType mat,
+                                      InputType mat2,
+                                      arma::uword remove_col)
+{
+  InputType mat3 = mat - mat2;
+  mat3 = arma::abs(mat3);
+  //logger::logger->info("Final prediction matrix {}", mat3);
+  mat3.shed_col(remove_col);
   arma::colvec vec = arma::sum(mat3, 1);
   vec = arma::reverse(vec);
   logger::logger_->info("Sum of distances: {}", vec);
