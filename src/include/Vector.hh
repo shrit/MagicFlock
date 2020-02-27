c113fc367ec4a241ac62ba6c5dcbbc51e6a24dce
Omar Shrit <shrit@lri.fr>
2020-02-27 19:22:25 +0100

    Create a vector class to mange std::vector operation
    Signed-off-by: Omar Shrit <shrit@lri.fr>

---
+++ src/include/Vector.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#include <vector>
+
+class VectorHelper
+{
+  VectorHelper(){}
+
+  template<typename Arg>
+  Arg variance(std::vector<Arg> vec);
+
+  template<typename Arg>
+  Arg mean(std::vector<Arg> vec);
+
+  template<typename T>
+  long long unsigned int index_of_max_value(const std::vector<T>& vec);
+
+  template<typename T>
+  long long unsigned int index_of_min_value(const std::vector<T>& vec);  
+};
+
---
+++ src/include/Vector.hxx
@@ -0,0 +1,48 @@
+
+template<typename T>
+long long unsigned int
+VectorHelper::index_of_max_value(const std::vector<T>& vec)
+{
+  auto highest = std::max_element(vec.begin(), vec.end());
+  return std::distance(vec.begin(), highest);
+}
+
+template<typename T>
+long long unsigned int
+VectorHelper::index_of_min_value(const std::vector<T>& vec)
+{
+  auto highest = std::min_element(vec.begin(), vec.end());
+  return std::distance(vec.begin(), highest);
+}
+
+template<typename Arg>
+Arg
+VectorHelper::mean(std::vector<Arg> vec)
+{
+  size_t sz = vec.size();
+  if (sz == 0)
+    return -1;
+
+  return std::accumulate(vec.begin(), vec.end(), 0.0) / vec.size();
+}
+
+template<typename Arg>
+Arg
+VectorHelper::variance(std::vector<Arg> vec)
+{
+  /*  note that diff_f3_ was used here */
+  size_t sz = vec.size();
+  if (sz == 1)
+    return -1;
+
+  /*  Do not take the first value */
+  Arg mean = std::accumulate(vec.begin() + 1, vec.end(), 0.0) / vec.size();
+
+  return std::accumulate(vec.begin(),
+                         vec.end(),
+                         0.0,
+                         [&mean, &sz](double accumulator, const double& val) {
+                           return accumulator +
+                                  ((val - mean) * (val - mean) / (sz - 1));
+                         });
+}
