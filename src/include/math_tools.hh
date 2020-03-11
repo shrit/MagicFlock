57ad167a515788444a6e05647e1d267a3b0e7ba1
Omar Shrit <shrit@lri.fr>
2020-03-11 17:10:32 +0100

    Move most math tools functions into another one
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/include/math_tools.hh
+++ src/include/math_tools.hh
@@ -4,8 +4,6 @@
 #include <algorithm>
 #include <cmath>
 #include <map>
-#include <numeric>
-#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -24,55 +22,8 @@ class Math_tools
 public:
   Math_tools() {}
 
-  template<typename T>
-  std::map<unsigned int, double> distances_to_neighbors(
-    unsigned int id,
-    std::vector<unsigned int> nearest_neighbors,
-    std::vector<lt::position3D<T>> positions);
-
-  template<typename T>
-  double distance_a_2_b(std::vector<lt::position3D<T>> positions,
-                        unsigned int id_a,
-                        unsigned int id_b);
-
-  template<typename T>
-  bool is_good_shape(unsigned int id,
-                     std::vector<unsigned int> nearest_neighbors,
-                     std::vector<lt::position3D<T>> positions);
-
-  template<typename T>
-  std::map<T, T> get_histogram();
-
-  template<typename T>
-  void histogram(T times);
-
-  template<typename KeyType, typename ValueType>
-  std::pair<KeyType, ValueType> get_max_histogram(
-    const std::map<KeyType, ValueType>& x);
-
-  template<typename Arg, typename Arg2>
-  arma::colvec to_one_hot_encoding(Arg arg, Arg2 number_of_class);
-
-  template<typename Arg>
-  arma::uword from_one_hot_encoding(arma::Col<Arg> col_vec);
-
-  template<typename Arg>
-  std::vector<double> to_std_vector(Arg arg);
-
-  template<typename T1, typename T2>
-  std::vector<T2> map_to_vector(std::map<T1, T2>);
-
   template<typename T1, typename T2>
   arma::colvec map_to_arma(std::map<T1, T2>);
-
-  template<typename Arg>
-  bool hamming_distance_one_hot(std::vector<Arg> v1, std::vector<Arg> v2);
-
-  template<typename T>
-  int ecludian_distance(T d1, T d2);
-
-  template<typename T>
-  double traveled_distances(lt::position3D<T> pos_t, lt::position3D<T> pos_t_1);
 
   template<typename T>
   T pythagore_leg(T leg, T hypotenuse);
@@ -80,12 +31,6 @@ public:
   template<typename T>
   T pythagore_hypotenuse(T leg_1, T leg_2);
 
-private:
-  std::map<int, int> histo_;
-  double lower_threshold_ =
-    1; /*  1 meters minimum distance between quadrotors */
-  double upper_threshold_ =
-    8; /*  8 meters maximum distance between quadrotors */
 };
 
 #include "math_tools.hxx"
--- src/include/math_tools.hxx
+++ src/include/math_tools.hxx
@@ -1,57 +1,5 @@
 #pragma once
 
-template<typename T>
-bool
-Math_tools::is_good_shape(unsigned int id,
-                          std::vector<unsigned int> nearest_neighbors,
-                          std::vector<lt::position3D<T>> positions)
-{
-  bool good_shape = true;
-  std::vector<double> distances =
-    map_to_vector(distances_to_neighbors(id, nearest_neighbors, positions));
-  logger::logger_->debug("Distances to other quadrotors: {} ", distances);
-  if (std::any_of(distances.begin(), distances.end(), [&](const double& i) {
-        if ((i < lower_threshold_) or (i > upper_threshold_)) {
-          return true;
-        } else
-          return false;
-      })) {
-    good_shape = false;
-  }
-  return good_shape;
-}
-
-template<typename T>
-void
-Math_tools::histogram(T times)
-{
-  auto it = histo_.find(times);
-  if (it != histo_.end()) {
-    it->second = it->second + 1;
-  } else {
-    histo_.insert(std::pair<T, T>(times, 1));
-  }
-}
-
-template<typename T>
-std::map<T, T>
-Math_tools::get_histogram()
-{
-  return histo_;
-}
-
-template<typename KeyType, typename ValueType>
-std::pair<KeyType, ValueType>
-Math_tools::get_max_histogram(const std::map<KeyType, ValueType>& x)
-{
-  return *std::max_element(x.begin(),
-                           x.end(),
-                           [](const std::pair<KeyType, ValueType>& p1,
-                              const std::pair<KeyType, ValueType>& p2) {
-                             return p1.second < p2.second;
-                           });
-}
-
 /*  Simple implementation, need more logical one */
 template<typename Arg, typename Arg2>
 arma::colvec
@@ -75,61 +23,6 @@ Math_tools::from_one_hot_encoding(arma::Col<Arg> col_vec)
   return index = col_vec.index_max();
 }
 
-template<typename Arg>
-bool
-Math_tools::hamming_distance_one_hot(std::vector<Arg> v1, std::vector<Arg> v2)
-{
-  bool distance = false;
-  if (v1.size() == v2.size()) {
-    if (v1 == v2) {
-      distance = true;
-    }
-  } else {
-    logger::logger_->error(
-      "Can not calculate hamming on different size vectors");
-  }
-  return distance;
-}
-
-template<typename T>
-int
-Math_tools::ecludian_distance(T d1, T d2)
-{
-  double distance = std::sqrt(std::pow((d1), 2) - std::pow((d2), 2));
-  return distance;
-}
-
-template<typename Arg>
-std::vector<double>
-Math_tools::to_std_vector(Arg arg)
-{
-  std::vector<double> vec;
-  if (arg.is_empty()) {
-    logger::logger_->error("Can not convert empty vector to std vector");
-  }
-  if (std::is_same<Arg, arma::rowvec>::value) {
-    vec.resize(arg.n_cols);
-    vec = arma::conv_to<std::vector<double>>::from(arg);
-  } else if (std::is_same<Arg, arma::colvec>::value) {
-    vec.resize(arg.n_rows);
-    vec = arma::conv_to<std::vector<double>>::from(arg);
-  }
-  logger::logger_->debug("Converted to std vector: {}", vec);
-  return vec;
-}
-
-template<typename T1, typename T2>
-std::vector<T2>
-Math_tools::map_to_vector(std::map<T1, T2> m)
-{
-  std::vector<T2> vec;
-  for (typename std::map<T1, T2>::iterator it = m.begin(); it != m.end();
-       ++it) {
-    vec.push_back(it->second);
-  }
-  return vec;
-}
-
 template<typename T1, typename T2>
 arma::colvec
 Math_tools::map_to_arma(std::map<T1, T2> m)
@@ -142,59 +35,6 @@ Math_tools::map_to_arma(std::map<T1, T2> m)
   arma::colvec arma_vec = arma::conv_to<arma::colvec>::from(vec);
 
   return arma_vec;
-}
-
-template<typename T>
-double
-Math_tools::distance_a_2_b(std::vector<lt::position3D<T>> positions,
-                           unsigned int id_a,
-                           unsigned int id_b)
-{
-  lt::position3D<double> dist;
-  /*  Distance between a and b */
-  dist.x = positions.at(id_a).x - positions.at(id_b).x;
-  dist.y = positions.at(id_a).y - positions.at(id_b).y;
-  dist.z = positions.at(id_a).z - positions.at(id_b).z;
-
-  double distance = std::sqrt(std::pow((dist.x), 2) + std::pow((dist.y), 2) +
-                              std::pow((dist.z), 2));
-  return distance;
-}
-
-template<typename T>
-std::map<unsigned int, double>
-Math_tools::distances_to_neighbors(unsigned int id,
-                                   std::vector<unsigned int> nearest_neighbors,
-                                   std::vector<lt::position3D<T>> positions)
-{
-  std::map<unsigned int, double> distances;
-  for (auto&& i : nearest_neighbors) {
-    distances.insert({ i, distance_a_2_b(positions, id, i) });
-  }
-  return distances;
-}
-
-template<typename T>
-double
-Math_tools::traveled_distances(lt::position3D<T> pos_t,
-                               lt::position3D<T> pos_t_1)
-{
-  lt::position3D<double> dist;
-
-  /* Travelled distance between time steps */
-  dist.x = pos_t.x - pos_t_1.x;
-  dist.y = pos_t.y - pos_t_1.y;
-  dist.z = pos_t.z - pos_t_1.z;
-
-  double traveled_distance;
-
-  /*  Summing up vectors, the total is the distance travelled by each
-      agent during each trajectory */
-
-  /*  Distance travelled by the leader */
-  traveled_distance = std::sqrt(std::pow((dist.x), 2) + std::pow((dist.y), 2) +
-                                std::pow((dist.z), 2));
-  return traveled_distance;
 }
 
 template<typename T>
