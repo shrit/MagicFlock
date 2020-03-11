97c2ac327a534ff9245b6c61a818fb512ae99f54
Omar Shrit <shrit@lri.fr>
2020-03-11 17:17:30 +0100

    Add a separate check shape function
    Signed-off-by: Omar Shrit <shrit@lri.fr>

---
+++ src/check_swarm_shape.cc
@@ -0,0 +1,21 @@
+#include "include/check_swarm_shape.hh"
+
+bool
+CheckShape::is_good_shape(unsigned int id,
+                          std::vector<unsigned int> nearest_neighbors,
+                          std::vector<lt::position3D<double>> positions)
+{
+  bool good_shape = true;
+  std::vector<double> distances =
+    vec_.map_to_vector(dist_.distances_to_neighbors(id, nearest_neighbors, positions));
+  logger::logger_->debug("Distances to other quadrotors: {} ", distances);
+  if (std::any_of(distances.begin(), distances.end(), [&](const double& i) {
+        if ((i < lower_threshold_) or (i > upper_threshold_)) {
+          return true;
+        } else
+          return false;
+      })) {
+    good_shape = false;
+  }
+  return good_shape;
+}
---
+++ src/include/check_swarm_shape.hh
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+#include "global.hh"
+#include "Vector.hh"
+
+namespace lt = local_types;
+
+class CheckShape
+{
+public:
+  CheckShape();
+
+  bool is_good_shape(unsigned int id,
+                     std::vector<unsigned int> nearest_neighbors,
+                     std::vector<lt::position3D<double>> positions);
+private:
+
+  double lower_threshold_ =
+    1; /*  1 meters minimum distance between quadrotors */
+  double upper_threshold_ =
+    8; /*  8 meters maximum distance between quadrotors */
+
+  VectorHelper vec_;
+  ComputeDistance dist_;
+};
