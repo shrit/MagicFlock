41a1059a19db8dc69acaad7323092f6f70527d13
Omar Shrit <shrit@lri.fr>
2020-03-21 17:50:29 +0100

    Adding header vae file
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- examples/vae_ann/vae_ann.hh
+++ examples/vae_ann/vae_ann.hh
@@ -1,1 +1,33 @@
+#pragma once
 
+#include <mlpack/core.hpp>
+#include <ensmallen_bits/adam/adam_update.hpp>
+#include <ensmallen_bits/callbacks/callbacks.hpp>
+#include <mlpack/methods/ann/ffn.hpp>
+#include <mlpack/methods/ann/init_rules/glorot_init.hpp>
+#include <mlpack/methods/ann/layer/layer.hpp>
+#include <mlpack/methods/ann/loss_functions/mean_squared_error.hpp>
+
+#include <ILMR/data_set.hh>
+#include <ILMR/logger.hh>
+#include <ILMR/timer.hh>
+
+template<class simulator_t>
+class VAE
+{
+
+public:
+  VAE(std::string dataset_filename, std::shared_ptr<spdlog::logger> logger);
+
+  void run();
+  void regression();
+
+  VAE(VAE const&) = delete;
+  VAE(VAE&&) = default;
+
+private:
+  DataSet<simulator_t> dataset_;
+  std::shared_ptr<spdlog::logger> logger_;
+};
+
+#include "vae_ann.hxx"
