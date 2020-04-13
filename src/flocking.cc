59f4d722c4864238a853d1fc97aab6642e9413f7
Omar Shrit <shrit@lri.fr>
2020-04-13 22:30:21 +0200

    Finish the flocking model in theory, no tested yet
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/flocking.cc
+++ src/flocking.cc
@@ -4,11 +4,16 @@ Flocking::Flocking(double sepGain,
                    double cohGain,
                    double migGain,
                    double cutoffDist,
-                   std::vector<ignition::math::Vector3d> position_of_neighbors)
+                   ignition::math::Vector3d position,
+                   std::vector<ignition::math::Vector3d> position_of_neighbors,
+                   ignition::math::Vector3d destination_position)
   : separation_gain_(sepGain)
   , cohesion_gain_(cohGain)
   , migration_gain_(migGain)
   , cutoff_distance_(cutoffDist)
+  , position_(position)
+  , position_of_neighbors_(position_of_neighbors)
+  , destination_position_(destination_position)
 
 {
   number_of_neighbors_ = position_of_neighbors.size();
@@ -22,8 +27,7 @@ Flocking::cohesionVelocity()
   ignition::math::Vector3d total_sum{ 0, 0, 0 };
 
   for (std::size_t i = 0; i < position_of_neighbors_.size(); ++i) {
-    total_sum += position_of_neighbors_.at(i);
- 
+    total_sum += (position_ - position_of_neighbors_.at(i));
   }
   cohesionVelocity = total_sum * param;
   return cohesionVelocity;
@@ -33,17 +37,34 @@ ignition::math::Vector3d
 Flocking::separationVelocity()
 {
   ignition::math::Vector3d separationVelocity;
+  double param = -(separation_gain_ / number_of_neighbors_);
+  ignition::math::Vector3d total_sum{ 0, 0, 0 };
+  ignition::math::Vector3d r_sep{ 0, 0, 0 }, r_sep_norm_2{ 0, 0, 0 };
 
+  for (std::size_t i = 0; i < position_of_neighbors_.size(); ++i) {
+    r_sep = (position_ - position_of_neighbors_.at(i));
+    double d = r_sep.X() * r_sep.X() + r_sep.Y() * r_sep.Y() +
+               r_sep.Z() * r_sep.Z();
+
+    r_sep_norm_2.X() = r_sep.X() / d;
+    r_sep_norm_2.Y() = r_sep.Y() / d;
+    r_sep_norm_2.Z() = r_sep.Z() / d;
+
+    total_sum += r_sep_norm_2;
+  }
+  separationVelocity = total_sum * param;
   return separationVelocity;
 }
 
 ignition::math::Vector3d
 Flocking::migrationVelocity()
-{  
+{
   ignition::math::Vector3d migrationVelocity;
-
+  ignition::math::Vector3d r_mig;
+  r_mig = position_ - destination_position_;
+  r_mig.Normalize(); // This will do the entire operation of division
+  migrationVelocity = r_mig * migration_gain_;
   return migrationVelocity;
-
 }
 
 ignition::math::Vector3d
--- src/include/flocking.hh
+++ src/include/flocking.hh
@@ -11,7 +11,9 @@ public:
            double cohGain,
            double migGain,
            double cutoffDist,
-           std::vector<ignition::math::Vector3d> position_of_neighbors);
+           ignition::math::Vector3d position,
+           std::vector<ignition::math::Vector3d> position_of_neighbors,
+           ignition::math::Vector3d destination_position);
 
   ignition::math::Vector3d cohesionVelocity();
   ignition::math::Vector3d separationVelocity();
@@ -28,5 +30,7 @@ private:
   double migration_gain_;
   double cutoff_distance_;
   int number_of_neighbors_;
+  ignition::math::Vector3d position_;
   std::vector<ignition::math::Vector3d> position_of_neighbors_;
+  ignition::math::Vector3d destination_position_;
 };
