329a924881cdf203c25c71dcd85e6e173fe0eb05
Omar Shrit <shrit@lri.fr>
2020-04-14 12:50:48 +0200

    Move timer class
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- src/include/timer.hh
+++ src/include/timer.hh
@@ -1,17 +1,1 @@
-#pragma once
 
-#include <chrono>
-#include <string>
-
-class Timer
-{
-
-public:
-  Timer();
-  void start();
-  int stop();
-  std::string stop_and_get_time();
-
-private:
-  std::chrono::steady_clock::time_point starting_time_;
-};
