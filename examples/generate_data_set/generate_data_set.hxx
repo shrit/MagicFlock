1ea4322311a70a54d815c650de8cd93d527263d6
Omar Shrit <shrit@lri.fr>
2020-04-26 23:22:57 +0200

    Possible new way to start simulation
    Signed-off-by: Omar Shrit <shrit@lri.fr>

--- examples/generate_data_set/generate_data_set.hxx
+++ examples/generate_data_set/generate_data_set.hxx
@@ -7,8 +7,8 @@ Generator<QuadrotorType>::Generator(
   : episode_(0)
   , max_episode_(10000)
   , start_episode_(false)
-  , swarm_(std::move(quads))
-  , quadrotors_(std::move(quadrotors))
+  , swarm_(quadrotors)
+  , quadrotors_(quadrotors)
   , logger_(logger)
 {
   /*  Allow easier access and debugging to all quadrotors state */
@@ -23,99 +23,30 @@ template<class QuadrotorType>
 void
 Generator<QuadrotorType>::generate_trajectory()
 {
-  ActionGenerator<QuadrotorType> leader_generator(leader_);
-  ActionGenerator<QuadrotorType> follower_1_generator(follower_1_);
-  ActionGenerator<QuadrotorType> follower_2_generator(follower_2_);
-
   std::vector<std::thread> threads;
-
-  /*  Sample the state at time t */
-  follower_1_->sample_state();
-  follower_2_->sample_state();
-
-  /*  Generate action for the leader */
-  leader_->current_action(
-    leader_generator.generate_persistant_action(10, time_steps_.steps()));
-  leader_2_->current_action(leader_->current_action());
-
   /*  Do not allow follower to move, at this time step,
         block the follower and log not move*/
-  follower_1_->current_action(Actions::Action::NoMove);
-  follower_2_->current_action(Actions::Action::NoMove);
 
-  logger_->info("Charlie distances to others before leader actions, {}",
-                        follower_1_->distances_to_neighbors());
-
-  logger_->info("Bob distances to others before leader actions, {}",
-                        follower_2_->distances_to_neighbors());
-
-  logger_->info(
-    "Current action leader: {}",
-    leader_generator.action_to_str(leader_->current_action()));
   /*  Threading Quadrotors */
   threads.push_back(std::thread([&]() {
-    swarm_.one_quad_execute_trajectory(
-      leader_->id(), leader_->current_action(), leader_->speed(), 1000);
+
   }));
 
   /*  Threading Quadrotors */
   threads.push_back(std::thread([&]() {
-    swarm_.one_quad_execute_trajectory(
-      leader_2_->id(), leader_2_->current_action(), leader_2_->speed(), 1000);
+
   }));
-
-  /* We need to wait until the Quadrotors finish their actions */
-  std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-
-  logger_->info("Charlie distances to others after leader actions, {}",
-                        follower_1_->distances_to_neighbors());
-
-  logger_->info("Bob distances to others after leader actions, {}",
-                        follower_2_->distances_to_neighbors());
 
   /* Get the next state at time t + 1  */
   logger_->info("Sampling states at t +1");
 
-  follower_1_->sample_state();
-  follower_2_->sample_state();
+  threads.push_back(std::thread([&]() {}));
 
-  follower_1_->current_action(follower_1_generator.generate_random_action());
-  follower_2_->current_action(follower_2_generator.generate_random_action());
-
-  threads.push_back(std::thread([&]() {
-    logger_->info(
-      "Current action follower 1: {}",
-      follower_1_generator.action_to_str(follower_1_->current_action()));
-    swarm_.one_quad_execute_trajectory(follower_1_->id(),
-                                       follower_1_->current_action(),
-                                       follower_1_->speed(),
-                                       1000);
-  }));
-
-  threads.push_back(std::thread([&]() {
-    logger_->info(
-      "Current action follower 2: {}",
-      follower_2_generator.action_to_str(follower_2_->current_action()));
-    swarm_.one_quad_execute_trajectory(follower_2_->id(),
-                                       follower_2_->current_action(),
-                                       follower_2_->speed(),
-                                       1000);
-  }));
+  threads.push_back(std::thread([&]() {}));
 
   for (auto& thread : threads) {
     thread.join();
   }
-
-  /* We need to wait until the Quadrotors finish their actions */
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-  /* Get the next state at time t + 2  */
-  logger_->info("Sampling states at t+2");
-  follower_1_->sample_state();
-  follower_2_->sample_state();
-
-  /* We need to wait until the Quadrotors finish their actions */
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 }
 
 template<class QuadrotorType>
--- examples/generate_data_set/main.cc
+++ examples/generate_data_set/main.cc
@@ -34,41 +34,30 @@ main(int argc, char* argv[])
   ILMR::logger::create_library_logger(logger);
   spdlog::set_level(spdlog::level::debug);
 
-  /*  Gazebo simulator */
-  std::shared_ptr<Gazebo> gz = std::make_shared<Gazebo>(argc, argv);
+  using QuadrotorType = Quadrotor<Px4Device, GaussianNoise<arma::vec>>;
 
-  gz->subscribe_position_topic();
-  gz->publishe_model_reset();
- 
+
+  /*  Create a vector of quadrotors, each one has an id + a label  */
+  std::vector<std::shared_ptr<QuadrotorType>> quadrotors;
+  // quadrotors.emplace_back("leader");
+  // quadrotors.emplace_back("follower_1");
+  // quadrotors.emplace_back("follower_2");
+  // quadrotors.emplace_back("leader_2");
+  /*  Add neighbors list -> Variadic template by label*/
+
+  /*  Gazebo simulator */ 
+  std::shared_ptr<Gazebo<QuadrotorType>> gz =
+    std::make_shared<Gazebo<QuadrotorType>>(argc, argv, quadrotors);
+
+  gz->subsPosTimeTopic();
+  gz->subRxTopic();
+  gz->pubModelReset();
+  gz->spawn(
+        quadrotors, "config/gazebo/models/x340/x340.sdf", "config/models/x340/rcS");
   /* Wait for 10 seconds, Just to finish subscribe to
    * gazebo topics */
   std::this_thread::sleep_for(std::chrono::seconds(10));
-
-  /*  Create a vector of quadrotors, each one has an id + a label  */
-  using QuadrotorType = Quadrotor<Px4Device, GaussianNoise<arma::vec>>;
-  std::vector<QuadrotorType> quadrotors;
-  quadrotors.emplace_back("leader");
-  quadrotors.emplace_back("follower_1");
-  quadrotors.emplace_back("follower_2");
-  quadrotors.emplace_back("leader_2");
-  /*  Add neighbors list -> Variadic template by label*/
-  quadrotors.at(0).add_nearest_neighbor_id(1);
-  quadrotors.at(0).add_nearest_neighbor_id(2);
-  quadrotors.at(0).add_nearest_neighbor_id(3);
-
-  quadrotors.at(1).add_nearest_neighbor_id(0);
-  quadrotors.at(1).add_nearest_neighbor_id(2);
-  quadrotors.at(1).add_nearest_neighbor_id(3);
-
-  quadrotors.at(2).add_nearest_neighbor_id(0);
-  quadrotors.at(2).add_nearest_neighbor_id(1);
-  quadrotors.at(2).add_nearest_neighbor_id(3);
-
-  quadrotors.at(3).add_nearest_neighbor_id(0);
-  quadrotors.at(3).add_nearest_neighbor_id(1);
-  quadrotors.at(3).add_nearest_neighbor_id(2);
-
   /*  Generate a dataset  */
-  Generator<QuadrotorType> generator(quadrotors, logger);
-  generator.run();
+  // Generator<QuadrotorType> generator(quadrotors, logger);
+  // generator.run();
 }
