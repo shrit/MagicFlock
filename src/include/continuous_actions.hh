7ebc6ab1069b2425ec2d741216b1386c068b5204
Omar Shrit <shrit@lri.fr>
2020-05-07 17:09:17 +0200

    Starting point as a continuous_actions might exist
    Signed-off-by: Omar Shrit <shrit@lri.fr>

---
+++ src/include/continuous_actions.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <ignition/math6/ignition/math/Vector3.hh>
+
+class ContinuousActions
+{
+public:
+  
+ ContinuousActions();
+ ignition::math::Vector3d velocity_vector();
+
+};
