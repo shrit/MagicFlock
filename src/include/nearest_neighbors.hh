4d6040bc5f92d8e3cdfc0c0a51a12df097fdec2e
Omar Shrit <shrit@lri.fr>
2020-05-08 19:16:08 +0200

    Create a nearest_neighbors class which parse id from essid
    Signed-off-by: Omar Shrit <shrit@lri.fr>

---
+++ src/include/nearest_neighbors.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <map>
+#include <vector>
+
+#include "real_time_samples.hh"
+
+/* This class parse the number of NearestNeighbor 
+ *  by extracting their id from the a vector*/
+
+template<class NeighborType>
+class NearestNeighbors
+{
+public:
+  NearestNeighbors(std::vector<NeighborType> neighbors);
+  std::vector<unsigned int> search();
+
+private:
+  std::vector<NeighborType> neighbors_;
+};
+
+#include "nearest_neighbors.hxx"
---
+++ src/include/nearest_neighbors.hxx
@@ -0,0 +1,20 @@
+#pragma once
+
+template<class NeighborType>
+NearestNeighbors<NeighborType>::NearestNeighbors(
+  std::vector<NeighborType> neighbors)
+  : neighbors_(neighbors)
+{}
+
+template<class NeighborType>
+std::vector<unsigned int>
+NearestNeighbors<NeighborType>::search()
+{
+  std::vector<unsigned int> ids;
+  for (auto it : neighbors_) {
+    std::string name = it->name();
+    unsigned int id = static_cast<unsigned int> (std::atoi(name.back());
+    ids.push_back(id);
+  }
+  return ids;
+}
