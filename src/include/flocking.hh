8b78c4ae072afd3ebad5c9ebbac3f35c1d1d259e
Omar Shrit <omar@shrit.me>
2020-07-03 16:41:11 +0200

    Add article references
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/include/flocking.hh
+++ src/include/flocking.hh
@@ -1,7 +1,26 @@
+/*
+ * This file implment a modified version of Reynolds flocking model
+ * This implementation is described in the following article
+ * 
+ * For more details please see the following paper: 
+ * 
+ *  @ARTICLE{8798720,
+ *    author={F. {Schilling} and J. {Lecoeur} and F. {Schiano} and D. {Floreano}},
+ *    journal={IEEE Robotics and Automation Letters}, 
+ *    title={Learning Vision-Based Flight in Drone Swarms by Imitation}, 
+ *    year={2019},
+ *    volume={4},
+ *    number={4},
+ *    pages={4523-4530},}
+ *
+ * author: Omar Shrit <omar@shrit.me>
+ *
+ */
+
 #pragma once
 
-#include <ignition/math6/ignition/math/Vector3.hh>
-#include <ignition/math6/ignition/math/Vector4.hh>
+#include <ignition/math/Vector3.hh>
+#include <ignition/math/Vector4.hh>
 #include <vector>
 
 #include "logger.hh"
@@ -33,9 +52,15 @@ public:
 private:
   int number_of_neighbors_;
 
-  // These default values are reported in this paper:
-  // https://ieeexplore.ieee.org/document/8675652
-  ignition::math::Vector4d gains_{ 1, 1, 10, 100 };
+  /* These default values are reported in this paper:
+   * https://ieeexplore.ieee.org/document/8675652
+   * The vector values are the following: 
+     * X = 1  -> cohesion gain
+     * Y = 10 -> separation gain
+     * Z = 1  -> migration gain
+     * W = 100 -> max neighbor distance
+   */ 
+  ignition::math::Vector4d gains_{ 1, 10, 1, 100 };
   ignition::math::Vector3d position_;
   std::vector<ignition::math::Vector3d> position_of_neighbors_;
   ignition::math::Vector3d destination_position_;
