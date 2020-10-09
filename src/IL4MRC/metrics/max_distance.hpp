70181bcc99bb737bca72dc43ae70535d958b9c04
Omar Shrit <omar@shrit.me>
2020-10-09 19:30:28 +0200

    Add local and global distance calculation function
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/IL4MRC/metrics/max_distance.hpp
+++ src/IL4MRC/metrics/max_distance.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#include <vector>
 #include <limits>
+#include <vector>
 
-#include <ignition/math/Vector3.hh>
 #include <IL4MRC/util/logger.hpp>
+#include <ignition/math/Vector3.hh>
 
 using namespace ILMR;
 
@@ -12,25 +12,41 @@ template<class QuadrotorType>
 class MaxDistance
 {
 public:
-  MaxDistance() : max_distance_(std::numeric_limits<double>::min())
-  { /* Nothing to do here*/
+  MaxDistance()
+    : max_global_distance_(std::numeric_limits<double>::min())
+    , max_local_distance_(std::numeric_limits<double>::min())
+  {
+    /* Nothing to do here*/
   }
 
-  double check_distance(const std::vector<QuadrotorType>& quads)
+  double check_global_distance(const std::vector<QuadrotorType>& quads)
   {
-    double max_distance;
+    double max_global_distance;
     for (auto&& q : quads) {
       for (std::size_t i = 0; i < q.neighbor_positions().size(); ++i) {
-        max_distance = q.position().Distance(q.neighbor_positions().at(i));
-        if (max_distance < max_distance_) {
-          max_distance_ = max_distance;
+        max_global_distance =
+          q.position().Distance(q.neighbor_positions().at(i));
+        if (max_global_distance < max_global_distance_) {
+          max_global_distance_ = max_global_distance;
         }
       }
     }
-    return max_distance_;
+    return max_global_distance_;
+  }
+
+  double check_local_distance(const QuadrotorType& q)
+  {
+    double max_local_distance;
+    for (std::size_t i = 0; i < q.neighbor_positions().size(); ++i) {
+      max_local_distance = q.position().Distance(q.neighbor_positions().at(i));
+      if (max_local_distance < max_local_distance_) {
+        max_local_distance_ = max_local_distance;
+      }
+    }
+    return max_local_distance_;
   }
 
 private:
-  double max_distance_;
+  double max_global_distance_;
+  double max_local_distance_;
 };
-
--- src/IL4MRC/metrics/min_distance.hpp
+++ src/IL4MRC/metrics/min_distance.hpp
@@ -3,8 +3,8 @@
 #include <limits>
 #include <vector>
 
-#include <ignition/math/Vector3.hh>
 #include <IL4MRC/util/logger.hpp>
+#include <ignition/math/Vector3.hh>
 
 using namespace ILMR;
 
@@ -12,26 +12,42 @@ template<class QuadrotorType>
 class MinDistance
 {
 public:
-  MinDistance() : min_distance_(std::numeric_limits<double>::max())
-  { 
+  MinDistance()
+    : min_global_distance_(std::numeric_limits<double>::max())
+    , min_local_distance_(std::numeric_limits<double>::max())
+  {
     /* Nothing to do here*/
   }
 
-  double check_distance(const std::vector<QuadrotorType>& quads)
+  double check_global_distance(const std::vector<QuadrotorType>& quads)
   {
-    double min_distance;
+    double min_global_distance;
     for (auto&& q : quads) {
       for (std::size_t i = 0; i < q.neighbor_positions().size(); ++i) {
-        min_distance = q.position().Distance(q.neighbor_positions().at(i));
-        if (min_distance < min_distance_) {
-          min_distance_ = min_distance;
+        min_global_distance =
+          q.position().Distance(q.neighbor_positions().at(i));
+        if (min_global_distance < min_global_distance_) {
+          min_global_distance_ = min_global_distance;
         }
       }
     }
-    return min_distance_;
+    return min_global_distance_;
+  }
+
+  double check_local_distance(const QuadrotorType& q)
+  {
+    double min_local_distance;
+      for (std::size_t i = 0; i < q.neighbor_positions().size(); ++i) {
+        min_local_distance =
+          q.position().Distance(q.neighbor_positions().at(i));
+        if (min_local_distance < min_local_distance_) {
+          min_local_distance_ = min_local_distance;
+        }
+      }
+    return min_local_distance_;
   }
 
 private:
-  double min_distance_;
+  double min_global_distance_;
+  double min_local_distance_;
 };
-
