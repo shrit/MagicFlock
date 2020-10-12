47fe9a0551286747e9485fcbaac94e652b341939
Omar Shrit <omar@shrit.me>
2020-10-12 19:24:50 +0200

    Add mean and stddev function
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/IL4MRC/dists/gaussian_noise.hpp
+++ src/IL4MRC/dists/gaussian_noise.hpp
@@ -6,15 +6,21 @@ template<class ContainerType>
 class GaussianNoise
 {
 public:
-  GaussianNoise();
+  GaussianNoise()
+  {
+   // Nothing to do here
+  }
+
   GaussianNoise(double mean, double standard_deviation);
 
-  ContainerType ApplyNoise(ContainerType container);
+  double& mean() { return mean_; }
+  double& standard_deviation() { return standard_deviation_; }
+
+  ContainerType apply_noise(ContainerType container);
 
 private:
   double mean_ = 0;
   double standard_deviation_ = 0.19;
-  std::normal_distribution<double> normal_distribution_;
   std::random_device random_dev;
   std::mt19937 generator_;
 
--- src/IL4MRC/dists/gaussian_noise_impl.hpp
+++ src/IL4MRC/dists/gaussian_noise_impl.hpp
@@ -5,16 +5,17 @@ GaussianNoise<ContainerType>::GaussianNoise(double mean,
                                             double standard_deviation)
   : mean_(mean)
   , standard_deviation_(standard_deviation)
-  , normal_distribution_(mean_, standard_deviation_)
   , generator_(random_dev())
 {}
 
 template<class ContainerType>
 ContainerType
-GaussianNoise<ContainerType>::ApplyNoise(ContainerType container)
+GaussianNoise<ContainerType>::apply_noise(ContainerType container)
 {
+  std::normal_distribution<double> normal_distribution(mean_,
+                                                       standard_deviation_);
   for (int i = 0; i < container.size(); ++i) {
-    double random = normal_distribution_(generator_);
+    double random = normal_distribution(generator_);
     container[i] = container[i] + random;
   }
   return container;
