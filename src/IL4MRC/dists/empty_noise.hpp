dbe60ab8fffba7b2453b8c9c95dd58699f124450
Omar Shrit <omar@shrit.me>
2020-10-14 16:47:45 +0200

    Match functions with gaussian noise
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/IL4MRC/dists/empty_noise.hpp
+++ src/IL4MRC/dists/empty_noise.hpp
@@ -1,19 +1,22 @@
 #pragma once
 
-#include <random>
-
 template<class ContainerType>
 class EmptyNoise
 {
 public:
   EmptyNoise();
-  EmptyNoise(double mean, double standard_deviation);
+  EmptyNoise(double mean, double standard_deviation)
+    : mean_(mean)
+    , standard_deviation_(standard_deviation)
+  {}
 
-  ContainerType ApplyNoise(ContainerType container);
+  ContainerType apply_noise(ContainerType container) { return container; }
+
+  double mean() { return mean_; }
+  double standard_deviation() { return standard_deviation_; }
 
 private:
   double mean_ = 0;
-  double standard_deviation_ = 0.19;
+  double standard_deviation_ = 0;
 };
 
-#include "empty_noise_impl.hpp"
--- src/IL4MRC/dists/empty_noise_impl.hpp
+++ src/IL4MRC/dists/empty_noise_impl.hpp
@@ -1,14 +1,1 @@
-#pragma once
 
-template<class ContainerType>
-EmptyNoise<ContainerType>::EmptyNoise(double mean, double standard_deviation)
-  : mean_(mean)
-  , standard_deviation_(standard_deviation)
-{}
-
-template<class ContainerType>
-ContainerType
-EmptyNoise<ContainerType>::ApplyNoise(ContainerType container)
-{
-  return container;
-}
