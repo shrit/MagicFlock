5456354b2564da6e2329635954edb3c1eb7a2e42
Omar Shrit <omar@shrit.me>
2020-11-07 18:49:12 +0100

    Remove the Dropout from the network
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/IL4MRC/algorithms/sac_predictor_impl.hpp
+++ src/IL4MRC/algorithms/sac_predictor_impl.hpp
@@ -12,7 +12,7 @@ SACPredictor<EnvironmentType,
              PolicyType,
              QuadrotorType,
              ReplayType>::SACPredictor(QuadrotorType& quad)
-  : replayMethod_(32, 10000, 1, 12)
+  : replayMethod_(32, 10000, 1, 14)
   , quadrotor_(quad)
   , policyNetwork_(mlpack::ann::EmptyLoss<>(),
                    mlpack::ann::GaussianInitialization(0, 0.1))
@@ -37,29 +37,25 @@ SACPredictor<EnvironmentType,
              ReplayType>::SacNetwork()
 {
   // Set up the actor and critic networks.
-  mlpack::rl::ContinuousActionEnv::State::dimension = 12;
+  mlpack::rl::ContinuousActionEnv::State::dimension = 14;
   mlpack::rl::ContinuousActionEnv::Action::size = 3;
 
-  policyNetwork_.Add<mlpack::ann::Linear<>>(12, 200);
+  policyNetwork_.Add<mlpack::ann::Linear<>>(14, 200);
   policyNetwork_.Add<mlpack::ann::ReLULayer<>>();
   policyNetwork_.Add<mlpack::ann::Linear<>>(200, 200);
   policyNetwork_.Add<mlpack::ann::ReLULayer<>>();
-  policyNetwork_.Add<mlpack::ann::Dropout<>>(0.5);
   policyNetwork_.Add<mlpack::ann::Linear<>>(200, 200);
   policyNetwork_.Add<mlpack::ann::ReLULayer<>>();
-  policyNetwork_.Add<mlpack::ann::Dropout<>>(0.5);
   policyNetwork_.Add<mlpack::ann::Linear<>>(200, 3);
   policyNetwork_.Add<mlpack::ann::TanHLayer<>>();
   policyNetwork_.ResetParameters();
 
-  qNetwork_.Add<mlpack::ann::Linear<>>(12 + 3, 200);
+  qNetwork_.Add<mlpack::ann::Linear<>>(14 + 3, 200);
   qNetwork_.Add<mlpack::ann::ReLULayer<>>();
   qNetwork_.Add<mlpack::ann::Linear<>>(200, 200);
   qNetwork_.Add<mlpack::ann::ReLULayer<>>();
-  qNetwork_.Add<mlpack::ann::Dropout<>>(0.5);
   qNetwork_.Add<mlpack::ann::Linear<>>(200, 200);
   qNetwork_.Add<mlpack::ann::ReLULayer<>>();
-  qNetwork_.Add<mlpack::ann::Dropout<>>(0.5);
   qNetwork_.Add<mlpack::ann::Linear<>>(200, 1);
   qNetwork_.ResetParameters();
 
@@ -67,7 +63,7 @@ SACPredictor<EnvironmentType,
   config_.UpdateInterval() = 1;
 
   agent_ = std::make_unique<
-  mlpack::rl::SAC<EnvironmentType, NetworkType, UpdaterType, PolicyType>>(
+    mlpack::rl::SAC<EnvironmentType, NetworkType, UpdaterType, PolicyType>>(
     config_, qNetwork_, policyNetwork_, replayMethod_);
 
   agent_->Deterministic() = false;
@@ -85,15 +81,14 @@ SACPredictor<EnvironmentType,
              UpdaterType,
              PolicyType,
              QuadrotorType,
-             ReplayType>::
-  train(size_t& consecutiveEpisodes,
-        const size_t numSteps,
-        std::function<void()> execute_action,
-        std::function<double()> evaluate_reward,
-        std::function<bool()> examine_environment)
+             ReplayType>::train(size_t& consecutiveEpisodes,
+                                const size_t numSteps,
+                                std::function<void()> execute_action,
+                                std::function<double()> evaluate_reward,
+                                std::function<bool()> examine_environment)
 {
   // Set up the state and action space.
-  mlpack::rl::ContinuousActionEnv::State::dimension = 12;
+  mlpack::rl::ContinuousActionEnv::State::dimension = 14;
   mlpack::rl::ContinuousActionEnv::Action::size = 3;
   logger::logger_->info("Training for: {}", numSteps, " steps.");
   while (agent_->TotalSteps() < numSteps) {
@@ -107,7 +102,8 @@ SACPredictor<EnvironmentType,
 
       // Here we need to execute action, swarm_.one..etc
       agent_->SelectAction();
-      arma::colvec action = arma::conv_to<arma::colvec>::from(agent_->Action().action);
+      arma::colvec action =
+        arma::conv_to<arma::colvec>::from(agent_->Action().action);
       logger::logger_->info("The created action {}", action);
       quadrotor_.current_action().set_action(action);
 
@@ -116,19 +112,28 @@ SACPredictor<EnvironmentType,
         nextState; // replace it with an arma matrix
       nextState.Data() = quadrotor_.current_state().Data();
       logger::logger_->info("Agent NextState {}", nextState.Data());
+      
       double reward = evaluate_reward();
-      logger::logger_->info("Print reward value", reward);
+      logger::logger_->info("Print reward value {}", reward);
       isTerminal = examine_environment();
+
+      arma::colvec stop_action = { 0, 0, 0 };
+      quadrotor_.current_action().set_action(stop_action);
 
       replayMethod_.Store(
         agent_->State(), agent_->Action(), reward, nextState, isTerminal, 0.99);
+      logger::logger_->info(
+        "States, action, reward stored successfully, Terminal State: {}",
+        isTerminal);
       episodeReturn_ += reward;
       agent_->TotalSteps()++;
 
       if (agent_->Deterministic() ||
-          agent_->TotalSteps() < config_.ExplorationSteps())
+          agent_->TotalSteps() < config_.ExplorationSteps()) {
         continue;
+      }
 
+      logger::logger_->info("Updating the agent to learn");
       for (size_t i = 0; i < config_.UpdateInterval(); i++)
         agent_->Update();
     }
