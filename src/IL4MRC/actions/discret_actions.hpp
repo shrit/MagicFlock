f5e002f526e941c9dc6828d7288a6a7eee2ba334
Omar Shrit <omar@shrit.me>
2020-12-15 19:17:35 +0100

    Remove old non useful action
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/IL4MRC/actions/discret_actions.hpp
+++ src/IL4MRC/actions/discret_actions.hpp
@@ -52,42 +52,6 @@ public:
   /*  Random action generator */
   Action random_action_generator();
 
-  Action random_action_generator_with_only_opposed_condition(Action action);
-
-  Action random_action_generator_with_all_conditions(Action action);
-
-  std::tuple<DiscretActions::Action, DiscretActions::Action>
-  generate_followers_action_using_distance(double distance_t_1_b,
-                                           double distance_t_b,
-                                           double distance_t_1_c,
-                                           double distance_t_c,
-                                           double alti_diff_t);
-
-  DiscretActions::Action undo_action(DiscretActions::Action action);
-
-  Action generate_follower_action_using_oracle(double distances_t_1_b,
-                                               double distances_t_b,
-                                               double distance_t_1_c,
-                                               double distance_t_c,
-                                               double alti_diff_t);
-
-  Action generate_leader_action(bool change_leader_action,
-                                bool stop_going_down,
-                                Action last_action,
-                                Action current_action);
-
-  Action validate_leader_action(double distance_to_b,
-                                double distance_to_b_1,
-                                double distance_to_c,
-                                double distance_to_c_1,
-                                Action current_action);
-
-  Action validate_followers_action(std::vector<double> current_distances,
-                                   std::vector<double> last_distances,
-                                   Action before_2_last_action,
-                                   Action current_action);
-  double generate_real_random();
-
 protected:
   std::uniform_int_distribution<> distribution_int_;
   std::uniform_real_distribution<> distribution_real_;
