7b57b53ed84df5a50b173bd9f5be2e3e5a5f675d
Omar Shrit <omar@shrit.me>
2020-12-15 22:23:04 +0100

    Adding one hot encoding for quadrotors
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/IL4MRC/metrics/one_hot_encoding.hpp
+++ src/IL4MRC/metrics/one_hot_encoding.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <mlpack/prereqs.hpp>
 #include <IL4MRC/util/logger.hpp>
+#include <mlpack/prereqs.hpp>
 
 class OneHotEncoding
 {
@@ -10,6 +10,10 @@ public:
 
   template<typename Arg, typename Arg2>
   arma::colvec to_one_hot_encoding(Arg arg, Arg2 number_of_class);
+
+  template<typename Arg>
+  arma::colvec OneHotEncoding::to_one_hot_encoding(ignition::math::Vector3d vec,
+                                                   Arg number_of_class);
 
   template<typename Arg>
   arma::uword from_one_hot_encoding(arma::Col<Arg> col_vec);
--- src/IL4MRC/metrics/one_hot_encoding_impl.hpp
+++ src/IL4MRC/metrics/one_hot_encoding_impl.hpp
@@ -17,9 +17,52 @@ OneHotEncoding::to_one_hot_encoding(Arg arg, Arg2 number_of_class)
 }
 
 template<typename Arg>
+arma::colvec
+OneHotEncoding::to_one_hot_encoding(ignition::math::Vector3d vec,
+                                    Arg number_of_class)
+{
+  arma::colvec one_hot(number_of_class, arma::fill::zeros);
+  // The vector need to have round before being treated
+
+  if (vec.X() == 1 and vec.Y() == 0 && vec.Z() == 0) {
+    one_hot.at(0) = 1;
+  } else if (vec.X() == -1 and vec.Y() == 0 && vec.Z() == 0) {
+    one_hot.at(1) = 1;
+
+  } else if (vec.X() == 0 and vec.Y() == 1 && vec.Z() == 0) {
+    one_hot.at(2) = 1;
+
+  } else if (vec.X() == 0 and vec.Y() == -1 && vec.Z() == 0) {
+    one_hot.at(3) = 1;
+
+  } else if (vec.X() == 0 and vec.Y() == 0 && vec.Z() == 1) {
+    one_hot.at(4) = 1;
+
+  } else if (vec.X() == 0 and vec.Y() == 0 && vec.Z() == -1) {
+    one_hot.at(5) = 1;
+
+  } else if (vec.X() == 1 and vec.Y() == 1 && vec.Z() == 0) {
+    one_hot.at(6) = 1;
+
+  } else if (vec.X() == -1 and vec.Y() == -1 && vec.Z() == 0) {
+    one_hot.at(7) = 1;
+
+  } else if (vec.X() == 1 and vec.Y() == -1 && vec.Z() == 0) {
+    one_hot.at(8) = 1;
+
+  } else if (vec.X() == -1 and vec.Y() == 1 && vec.Z() == 0) {
+    one_hot.at(9) = 1;
+
+  } else if (vec.X() == 0 and vec.Y() == 0 && vec.Z() == 0) {
+    one_hot.at(10) = 1;
+  }
+
+  return one_hot;
+}
+
+template<typename Arg>
 arma::uword
 OneHotEncoding::from_one_hot_encoding(arma::Col<Arg> col_vec)
 {
   return index = col_vec.index_max();
 }
-
