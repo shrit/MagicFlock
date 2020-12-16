3654bf2377a585b41f1d30c3fc5068ed8a837a1b
Omar Shrit <omar@shrit.me>
2020-12-16 18:11:11 +0100

    Refactor the state predcitor function
    Signed-off-by: Omar Shrit <omar@shrit.me>

--- src/IL4MRC/algorithms/ann_state_predictor.hpp
+++ src/IL4MRC/algorithms/ann_state_predictor.hpp
@@ -29,34 +29,14 @@ template<class QuadrotorType>
 class AnnStatePredictor : public virtual AnnPredictor<QuadrotorType>
 {
 public:
-  AnnStatePredictor(
-    std::string full_path_to_model,
-    std::string model_name,
-    QuadrotorType& quad);
+  AnnStatePredictor(QuadrotorType& quad);
 
-  arma::vec best_predicted_state();
-
-  arma::Col<arma::uword> all_predicted_actions() const;
-
-  arma::mat predict();
-  
-  double real_time_loss();
-
-  arma::vec loss_vector() const;
   typename QuadrotorType::Action best_predicted_action();
-
+  typename QuadrotorType::Action best_predicted_mig_action();
+  typename QuadrotorType::Action best_predicted_cohsep_action();
+  
   AnnStatePredictor(AnnStatePredictor const&) = delete;
   AnnStatePredictor(AnnStatePredictor&&) = default;
-
-private:
-  arma::mat labels_;
-  std::string model_path_;
-  std::string model_name_;
-  double real_time_loss_;
-  arma::vec loss_vector_;
-  arma::uword best_action_index_;
-  typename QuadrotorType::Action best_action_follower_;
-  arma::Col<arma::uword> all_predicted_actions_;
 };
 
 #include "ann_state_predictor_impl.hpp"
--- src/IL4MRC/algorithms/ann_state_predictor_impl.hpp
+++ src/IL4MRC/algorithms/ann_state_predictor_impl.hpp
@@ -2,127 +2,151 @@
 
 template<class QuadrotorType>
 AnnStatePredictor<QuadrotorType>::AnnStatePredictor(
-  std::string full_path_to_model,
-  std::string model_name,
   QuadrotorType& quad)
   : AnnPredictor<QuadrotorType>(quad)
-  , real_time_loss_(0)
-  , model_path_(full_path_to_model)
-  , model_name_(model_name)
 {
   // Nothing to do here.
 }
 
 template<class QuadrotorType>
 arma::mat
-AnnStatePredictor<QuadrotorType>::predict()
+AnnStatePredictor<QuadrotorType>::best_predicted_action(std::string model_path,
+                                                        std::string model_name)
 {
   mlpack::ann::FFN<mlpack::ann::MeanSquaredError<>,
                    mlpack::ann::GlorotInitialization>
     regression_model;
 
-  mlpack::data::Load(model_path_, model_name_, regression_model, true);
-  arma::mat features = this->create_features_matrix();
+  mlpack::data::Load(model_path, model_name, regression_model, true);
+  arma::mat features = this->state_features_matrix();
 
-  if constexpr (std::is_same<typename QuadrotorType::Action,
-                             ContinuousActions>::value) {
-    labels_.clear();
+  /**
+   * In this function we predict all the possible next states using
+   * regression model, each next state is related to a specific action.
+   * Finally, we do an argmin between the original state and all of the
+   * next states observed here.
+   * The action which correspond to the best state is returned by argmin
+   * and returned finally by the best action function
+   * The objective of this function is to predict only next states
+   * This in only valid in the case of discret actions.
+   */
+  arma::mat labels;
 
-    logger::logger_->info("Size of State features matrix: {}",
-                          arma::size(features));
-    // logger::logger_->info("State data matrix:\n {}", features.t());
-    regression_model.Predict(features, labels_);
+  logger::logger_->info("Size of State features matrix: {}",
+                        arma::size(features));
+  // logger::logger_->info("State data matrix:\n {}", features.t());
+  regression_model.Predict(features, labels);
 
-    // logger::logger_->info("State prediction matrix:\n {}", labels_.t());
-    arma::colvec prefect_data = {2,0,0,2,0,0,2,0,0,2,0,0,2,0,0,2,0,0};
-    arma::colvec reduced_prefect_data = {2,2,2,2,0,0};
-    this->quad_.all_states().at(0).Data() = prefect_data;
-    arma::mat original_state_matrix =
-      this->create_state_matrix(this->quad_.all_states().at(0), labels_.n_cols);
-    Argmin<arma::mat, arma::uword> argmin(original_state_matrix, labels_, 1);
+  // logger::logger_->info("State prediction matrix:\n {}", labels_.t());
+  arma::colvec prefect_data = { 2, 0, 0, 2, 0, 0, 2, 0, 0,
+                                2, 0, 0, 2, 0, 0, 2, 0, 0 };
+  this->quad_.all_states().at(0).Data() = prefect_data;
+  arma::mat original_state_matrix =
+    this->create_state_matrix(this->quad_.all_states().at(0), labels.n_cols);
+  Argmin<arma::mat, arma::uword> argmin(original_state_matrix, labels, 1);
 
-    best_action_index_ = argmin.min_index();
-    logger::logger_->info("Index of best action: {}", best_action_index_);
+  best_action_index_ = argmin.min_index();
+  logger::logger_->info("Index of best action: {}", best_action_index_);
 
-    /*  Get the follower action now !! and store it directly */
-    best_action_follower_ = this->action_.to_action(best_action_index_);
+  /*  Get the follower action now !! and store it directly */
+  typename QuadrotorType::Action best_action;
+  best_action = this->action_.to_action(best_action_index_);
 
-  } else if constexpr (std::is_same<typename QuadrotorType::Action,
-                                    DiscretActions>::value) {
-
-    /**
-     * In this function we predict all the possible next states using
-     * regression model, each next state is related to a specific action.
-     * Finally, we do an argmin between the original state and all of the
-     * next states observed here.
-     * The action which correspond to the best state is returned by argmin
-     * and returned finally by the best action function
-     * The objective of this function is to predict only next states
-     * This in only valid in the case of discret actions.
-     */
-    labels_.clear();
-    regression_model.Predict(features, labels_);
-
-    logger::logger_->info("Size of State features matrix: {}",
-                          arma::size(features));
-    logger::logger_->info("State data matrix:\n {}", features.t());
-    logger::logger_->info("State prediction matrix:\n {}", labels_.t());
-
-    arma::mat original_state_matrix =
-      this->create_state_matrix(this->quad_.all_states().at(0), labels_.n_cols);
-
-    Argmin<arma::mat, arma::uword> argmin(original_state_matrix, labels_, 1);
-
-    best_action_index_ = argmin.min_index();
-    logger::logger_->info("Index of best action: {}", best_action_index_);
-
-    /*  Get the follower action now !! and store it directly */
-    best_action_follower_ = this->action_.int_to_action(best_action_index_);
-
-    arma::Col<arma::uword> temp;
-    temp << best_action_index_;
-    all_predicted_actions_.insert_rows(all_predicted_actions_.n_rows, temp);
-  }
-  return labels_;
-}
-
-template<class QuadrotorType>
-arma::vec
-AnnStatePredictor<QuadrotorType>::best_predicted_state()
-{
-  /* Return the best state based on the best action discovered in predict */
-  return labels_.col(best_action_index_);
+  return best_action;
 }
 
 template<class QuadrotorType>
 typename QuadrotorType::Action
-AnnStatePredictor<QuadrotorType>::best_predicted_action()
+AnnStatePredictor<QuadrotorType>::best_predicted_cohsep_action(
+  std::string model_path,
+  std::string model_name)
+
 {
-  /* Return the best action using the above predict function */
-  predict();
-  this->quad_.current_predicted_state().Data() = best_predicted_state();
-  return best_action_follower_;
+  mlpack::ann::FFN<mlpack::ann::MeanSquaredError<>,
+                   mlpack::ann::GlorotInitialization>
+    regression_model;
+
+  mlpack::data::Load(model_path, model_name, regression_model, true);
+  arma::mat features = this->cohsep_vel_state_features_matrix();
+
+  /**
+   * In this function we predict all the possible next states using
+   * regression model, each next state is related to a specific action.
+   * Finally, we do an argmin between the original state and all of the
+   * next states observed here.
+   * The action which correspond to the best state is returned by argmin
+   * and returned finally by the best action function
+   * The objective of this function is to predict only next states
+   * This in only valid in the case of discret actions.
+   */
+  arma::mat labels;
+
+  logger::logger_->info("Size of State features matrix: {}",
+                        arma::size(features));
+  // logger::logger_->info("State data matrix:\n {}", features.t());
+  regression_model.Predict(features, labels);
+
+  // logger::logger_->info("State prediction matrix:\n {}", labels_.t());
+  arma::colvec prefect_data = { 2, 0, 0, 2, 0, 0, 2, 0, 0,
+                                2, 0, 0, 2, 0, 0, 2, 0, 0 };
+  this->quad_.all_states().at(0).Data() = prefect_data;
+  arma::mat original_state_matrix =
+    this->create_state_matrix(this->quad_.all_states().at(0), labels.n_cols);
+  Argmin<arma::mat, arma::uword> argmin(original_state_matrix, labels, 1);
+
+  best_action_index_ = argmin.min_index();
+  logger::logger_->info("Index of best action: {}", best_action_index_);
+
+  /*  Get the follower action now !! and store it directly */
+  typename QuadrotorType::Action best_action;
+  best_action = this->action_.to_action(best_action_index_);
+  return best_action;
 }
 
 template<class QuadrotorType>
-arma::Col<arma::uword>
-AnnStatePredictor<QuadrotorType>::all_predicted_actions() const
+typename QuadrotorType::Action
+AnnStatePredictor<QuadrotorType>::best_predicted_mig_action(
+  std::string model_path,
+  std::string model_name)
 {
-  /* Return a vector of all best predicted actions in the past */
-  return all_predicted_actions_;
+  mlpack::ann::FFN<mlpack::ann::MeanSquaredError<>,
+                   mlpack::ann::GlorotInitialization>
+    regression_model;
+
+  mlpack::data::Load(model_path, model_name, regression_model, true);
+  arma::mat features = this->mig_vel_state_features_matrix();
+
+  /**
+   * In this function we predict all the possible next states using
+   * regression model, each next state is related to a specific action.
+   * Finally, we do an argmin between the original state and all of the
+   * next states observed here.
+   * The action which correspond to the best state is returned by argmin
+   * and returned finally by the best action function
+   * The objective of this function is to predict only next states
+   * This in only valid in the case of discret actions.
+   */
+  arma::labels;
+
+  logger::logger_->info("Size of State features matrix: {}",
+                        arma::size(features));
+  // logger::logger_->info("State data matrix:\n {}", features.t());
+  regression_model.Predict(features, labels);
+
+  // logger::logger_->info("State prediction matrix:\n {}", labels_.t());
+  arma::colvec prefect_data = { 2, 0, 0, 2, 0, 0, 2, 0, 0,
+                                2, 0, 0, 2, 0, 0, 2, 0, 0 };
+  this->quad_.all_states().at(0).Data() = prefect_data;
+  arma::mat original_state_matrix =
+    this->create_state_matrix(this->quad_.all_states().at(0), labels.n_cols);
+  Argmin<arma::mat, arma::uword> argmin(original_state_matrix, labels, 1);
+
+  best_action_index_ = argmin.min_index();
+  logger::logger_->info("Index of best action: {}", best_action_index_);
+
+  /*  Get the follower action now !! and store it directly */
+  typename QuadrotorType::Action best_action;
+  best_action = this->action_.to_action(best_action_index_);
+  return best_action;
 }
 
-template<class QuadrotorType>
-double
-AnnStatePredictor<QuadrotorType>::real_time_loss()
-{
-  real_time_loss_ = this->compute_real_loss(labels_, best_action_index_);
-  return real_time_loss_;
-}
-
-template<class QuadrotorType>
-arma::vec
-AnnStatePredictor<QuadrotorType>::loss_vector() const
-{
-  return loss_vector_;
-}
