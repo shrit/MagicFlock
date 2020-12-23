a8f124f67ca487d3d3f46c3126e3cb1e22581679
Omar Shrit <omar@shrit.me>
2020-12-23 22:35:46 +0100

    Add scheduler class
    Signed-off-by: Omar Shrit <omar@shrit.me>

---
+++ src/IL4MRC/util/scheduler.hpp
@@ -0,0 +1,42 @@
+#include "timer.hpp"
+
+class Scheduler
+{
+
+public:
+  Scheduler();
+
+  void start() { timer_.start(); }
+
+  void each_interval(int sec,
+                     bool& value,
+                     std::function<void(void)> execute_function)
+  {
+    elapsed_time_ = timer_.stop();
+    logger_->info("Model time in seconds {}", elapsed_time_);
+
+    if (elapsed_time_ > passed_time_ + sec) {
+      logger_->info("Interval has passed change the value", elapsed_time_);
+      passed_time_ = elapsed_time_;
+      if (value == true)
+        value = false;
+      else {
+        value = true;
+      }
+      execute_function();
+    }
+  }
+
+  void change_after(int sec, bool& value) {}
+
+  void reset()
+  {
+    elapsed_time_ = 0;
+    passed_time_ = 0;
+  }
+
+private:
+  Timer timer_;
+  double elapsed_time_, passed_time_;
+  int counter_;
+};
