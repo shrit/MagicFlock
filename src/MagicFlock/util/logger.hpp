64f382f7aebfd1a194acab727c68b544ab9cee67
Omar Shrit <omar@avontech.fr>
2023-11-29 13:56:15 +0100

    Refactor logger to make it easier to use and header only
    Signed-off-by: Omar Shrit <omar@avontech.fr>

--- src/MagicFlock/util/logger.hpp
+++ src/MagicFlock/util/logger.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-# include <memory>
-# include <sstream>
-# include <thread>
+#include <memory>
+#include <sstream>
+#include <thread>
+#include <filesystem>
 
 /*  Speed log include  */
 #include <spdlog/spdlog.h>
@@ -13,8 +14,7 @@
 #include <spdlog/fmt/ostr.h>
 
 /*  Boost and file system includes */
-# include <experimental/filesystem>
-# include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 /*  Init the logging system */
 namespace ILMR
@@ -22,10 +22,49 @@ namespace ILMR
   class logger
   {
   public:
-    static std::shared_ptr<spdlog::logger> setup_logger(std::vector<spdlog::sink_ptr> sinks);
-    static std::shared_ptr<spdlog::logger> init();
-    static void create_library_logger(std::shared_ptr<spdlog::logger> logger);
-    static std::shared_ptr<spdlog::logger> logger_;
+    static std::shared_ptr<spdlog::logger> setup_logger(std::vector<spdlog::sink_ptr> sinks)
+    {
+      spdlog::init_thread_pool(8192, 1);
+      auto logger = std::make_shared<spdlog::async_logger>(
+        "IL",
+        sinks.begin(),
+        sinks.end(),
+        spdlog::thread_pool(),
+        spdlog::async_overflow_policy::block);
+        spdlog::register_logger(logger);
+
+      return logger;
+    }
+
+    static std::shared_ptr<spdlog::logger> init()
+    {
+      boost::posix_time::ptime now = boost::posix_time::second_clock::local_time();
+      std::stringstream date, time;
+      date << now.date();
+      time << now.time_of_day();
+
+      std::filesystem::create_directory("../log");
+      std::filesystem::create_directory("../log/" + date.str());
+      std::string log_file_name = "../log/" + date.str() + "/" + time.str();
+
+      auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+      console_sink->set_level(spdlog::level::info);
+
+      auto rotating_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+        log_file_name, 1024 * 1024 * 10, 3);
+      rotating_sink->set_level(spdlog::level::trace);
+
+      std::vector<spdlog::sink_ptr> sinks{ rotating_sink, console_sink };
+      auto logger = setup_logger(sinks);
+      return logger;
+    }
+
+    static void create_library_logger(std::shared_ptr<spdlog::logger> logger)
+    {
+      logger_ = logger;
+    }
+
+    inline static std::shared_ptr<spdlog::logger> logger_;
   };
 }
 
